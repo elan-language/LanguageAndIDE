f825f164c5f3cc8319537337546cd0a85ba7e4d1
Richard Pawson <rpawson@nakedobjects.net>
2024-01-17 16:08:25 +0000

    #22 Add Throw statement

--- elan/src/test/milestone_1.test.ts
+++ elan/src/test/milestone_1.test.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { assertAreEqual } from './testHelpers';
-import { T00_emptyFile, T03_mainWithVarAndSet, T04_allGlobals } from './testFrameFunctions';
+import { T00_emptyFile, T03_mainWithAllStatements, T04_allGlobals } from './testFrameFunctions';
 
 suite('Milestone 1 - Html rendering of code from model', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -8,8 +8,8 @@ suite('Milestone 1 - Html rendering of code from model', () => {
 	test('Test Empty File', (done) => {
 		assertAreEqual(done, "T00_emptyFile.html", T00_emptyFile);
 	});
-	test('Test Main With Var and Set', (done) => {
-		assertAreEqual(done, "T03_mainWithVarAndSet.html", T03_mainWithVarAndSet);
+	test('Test Main With All Statements', (done) => {
+		assertAreEqual(done, "T03_mainWithAllStatements.html", T03_mainWithAllStatements);
 	});
 	test('Test All Globals', (done) => {
 		assertAreEqual(done, "T04_allGlobals.html", T04_allGlobals);
