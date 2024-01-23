cf61c0fa6f8a1784ac3593c129f2d6b7f8052ef0
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-01-23 11:17:58 +0000

    #23 fix property selection

--- elan/src/frames/class-members/property.ts
+++ elan/src/frames/class-members/property.ts
@@ -27,6 +27,6 @@ export class Property extends AbstractFrame implements Member {
     }
 
     renderAsHtml(): string {
-        return `<property class="" id='${this.htmlId}' tabindex="0">${this.modifier()}<keyword>property </keyword>${this.name.renderAsHtml()} ${this.type.renderAsHtml()}</property>`;
+        return `<property class="${this.cls()}" id='${this.htmlId}' tabindex="0">${this.modifier()}<keyword>property </keyword>${this.name.renderAsHtml()} ${this.type.renderAsHtml()}</property>`;
     }
 } 
