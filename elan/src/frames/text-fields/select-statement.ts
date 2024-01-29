51efa647697af2c1582fd00090e8a9ada0ff0100
Richard Pawson <rpawson@nakedobjects.net>
2024-01-29 18:31:24 +0000

    #62

--- elan/src/frames/text-fields/select-statement.ts
+++ elan/src/frames/text-fields/select-statement.ts
@@ -2,13 +2,25 @@ import { Statement } from "../statements/statement";
 import { Frame } from "../frame";
 import { Text } from "./text";
 import { singleIndent } from "../helpers";
+import {FrameWithStatements} from "../frame-with-statements";
+import { Call } from "../statements/call";
+import { Each } from "../statements/each";
+import { For } from "../statements/for";
+import { IfThen } from "../statements/if-then";
+import { Print } from "../statements/print";
+import { Repeat } from "../statements/repeat";
+import { SetStatement } from "../statements/set-statement";
+import { Throw } from "../statements/throw";
+import { TryCatch } from "../statements/try-catch";
+import { Variable } from "../statements/variable";
+import { While } from "../statements/while";
 
 export class SelectStatement extends Text implements Statement {  
     isStatement = true;
 
     constructor(parent: Frame) {
         super(parent);
-        this.setPrompt("statement");
+        this.setPrompt("call each for if print repeat set switch throw try var while");
     }
 
     getPrefix(): string {
@@ -25,5 +37,78 @@ export class SelectStatement extends Text implements Statement {
 
     renderAsSource(): string {
         return `${this.indent()}`;
+    }
+
+    private addBeforeAndSelect(member: Statement) {
+        (this.parent as FrameWithStatements).addStatementBefore(member, this);
+        this.deselectAll(); //TODO should happen automatically
+        member.select(true);
+    }
+
+    //call each for if print repeat set switch throw try var while
+    enterText(char: string): void {
+        var empty = this.text ==="";
+        if (empty && (char ==='c')) {
+            var c = new Call(this.parent);
+            this.addBeforeAndSelect(c);
+            return;
+        }
+       /* if (empty && (char ==='e')) {
+            var e = new Each(this.parent);
+            this.addBeforeAndSelect(e);
+            return;
+        } 
+        if (empty && (char ==='f')) {
+            var f = new For(this.parent);
+            this.addBeforeAndSelect(f);
+            return;
+        }
+        if (empty && (char ==='i')) {
+            var i = new IfThen(this.parent);
+            this.addBeforeAndSelect(i);
+            return;
+        }*/
+        if (empty && (char ==='p')) {
+            var p = new Print(this.parent);
+            this.addBeforeAndSelect(p);
+            return;
+        }
+       /* if (empty && (char ==='r')) {
+            var r = new Repeat(this.parent);
+            this.addBeforeAndSelect(r);
+            return;
+        }*/
+        if (this.text ==="s" && (char ==='e')) {
+            var se = new SetStatement(this.parent);
+            this.text = "";
+            this.addBeforeAndSelect(se);
+            return;
+        }
+        if (this.text ==="s" && (char ==='w')) {
+            throw new Error("Not implemented");
+        }
+        if (this.text ==="t" && (char ==='h')) {
+            var th = new Throw(this.parent);
+            this.text = "";
+            this.addBeforeAndSelect(th);
+            return;
+        }
+        /*if (this.text ==="t" && (char ==='r')) {
+            var tr = new TryCatch(this.parent);
+            this.text = "";
+            this.addBeforeAndSelect(tr);
+            return;
+        }*/
+        if (empty && (char ==='v')) {
+            var m = new Variable(this.parent);
+            this.addBeforeAndSelect(m);
+            return;
+        }
+        /*if (empty && (char ==='w')) {
+            var w = new While(this.parent);
+            this.addBeforeAndSelect(w);
+            return;
+        }*/
+        super.enterText(char);
     }
 } 
