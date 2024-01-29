d72032a1f464f5b903255d4911f804f1c51b9401
Richard Pawson <rpawson@nakedobjects.net>
2024-01-29 18:08:32 +0000

    #62

--- elan/src/frames/text-fields/select-member.ts
+++ elan/src/frames/text-fields/select-member.ts
@@ -1,14 +1,19 @@
 import { Frame } from "../frame";
 import { singleIndent } from "../helpers";
-import { Member} from "../class-members/member";
 import { Text } from "./text";
+import { Class } from "../globals/class";
+import { FunctionMethod } from "../class-members/function-method";
+import { Member } from "../class-members/member";
+import { Property } from "../class-members/property";
+import { ProcedureMethod } from "../class-members/procedure-method";
+
 
 export class SelectMember extends Text implements Member {
     isMember = true;
     
     constructor(parent: Frame) {
         super(parent);
-        this.setPrompt("member");
+        this.setPrompt("function procedure property");
     }
 
     getPrefix(): string {
@@ -21,5 +26,33 @@ export class SelectMember extends Text implements Member {
 
     renderAsSource(): string {
         return `${singleIndent()}member\r\n`;
+    }
+
+    private addBeforeAndSelect(member: Member) {
+        (this.parent as Class).addMemberBefore(member, this);
+        this.deselectAll(); //TODO should happen automatically
+        member.select(true);
+    }
+
+    enterText(char: string): void {
+        var empty = this.text ==="";
+        if (empty && (char ==='f')) {
+            var m = new FunctionMethod(this.parent);
+            this.addBeforeAndSelect(m);
+            return;
+        }
+        if (this.text === "pro" && char ==="c") {
+            var proc= new ProcedureMethod(this.parent);
+            this.text = "";
+            this.addBeforeAndSelect(proc);
+            return;
+        }
+        if (this.text === "pro" && char ==="p") {
+            var prop= new Property(this.parent);
+            this.text = "";
+            this.addBeforeAndSelect(prop);
+            return;
+        }
+        super.enterText(char);
     }
 } 
