978b476d16a659fe96375a28d9b97a2d2adff9ab
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-03-12 15:11:32 +0000

    #154 complete while

--- elan/src/test/compiler/T_10_WhileLoop.test.ts
+++ elan/src/test/compiler/T_10_WhileLoop.test.ts
@@ -34,5 +34,128 @@ export async function main() {
     await assertObjectCodeExecutes(fileImpl, "10");
   });
 
+  test('Pass_innerLoop', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var t set to 0
+  var x set to 0
+  while x < 3
+    var y set to 0
+      while y < 4
+        set y to y + 1
+        set t to t + 1
+      end while
+      set x to x + 1
+  end while
+  print t
+end main`;
+
+    const objectCode = `var system : any; export function _inject(l : any) { system = l; };
+export async function main() {
+  var t = 0;
+  var x = 0;
+  while (x < 3) {
+    var y = 0;
+    while (y < 4) {
+      y = y + 1;
+      t = t + 1;
+    }
+    x = x + 1;
+  }
+  system.print(system.asString(t));
+}
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "12");
+  });
+
+  test('Fail_noEnd', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x = 0
+  while x < 10
+    set x to x + 1
+ end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_variableNotPredefined', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  while x < 10
+    set x to x + 1
+  end while
+ end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_variableDefinedInWhile', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  while var x < 10
+    set x to x + 1
+  end while
+ end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_noCondition', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to 0
+  while
+    set x to x + 1
+  end while
+ end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_while_do', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to 0
+  while x < 10
+    set x to x + 1
+  do
+ end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
 
 });
