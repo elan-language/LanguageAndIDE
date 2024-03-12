58b5a8ec24570a3e7f42f4ed03377c416737c8d8
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-03-12 14:08:47 +0000

    #164 more tests

--- elan/src/test/compiler/T_9_Conditions.test.ts
+++ elan/src/test/compiler/T_9_Conditions.test.ts
@@ -28,5 +28,83 @@ export async function main() {
     assertStatusIsValid(fileImpl);
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeExecutes(fileImpl, "truefalsefalse");
-});
+  });
+
+  test('Pass_greaterThan', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  print 3 > 4
+  print 3 > 2
+  print 3 > 3
+end main`;
+
+    const objectCode = `var system : any; export function _inject(l : any) { system = l; };
+export async function main() {
+  system.print(system.asString(3 > 4));
+  system.print(system.asString(3 > 2));
+  system.print(system.asString(3 > 3));
+}
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "falsetruefalse");
+  });
+
+  test('Pass_lessThanOrEqual', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  print 3 <= 4
+  print 3 <= 2
+  print 3 <= 3
+end main`;
+
+    const objectCode = `var system : any; export function _inject(l : any) { system = l; };
+export async function main() {
+  system.print(system.asString(3 <= 4));
+  system.print(system.asString(3 <= 2));
+  system.print(system.asString(3 <= 3));
+}
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "truefalsetrue");
+  });
+
+  test('Pass_greaterThanOrEqual', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  print 3 >= 4
+  print 3 >= 2
+  print 3 >= 3
+end main`;
+
+    const objectCode = `var system : any; export function _inject(l : any) { system = l; };
+export async function main() {
+  system.print(system.asString(3 >= 4));
+  system.print(system.asString(3 >= 2));
+  system.print(system.asString(3 >= 3));
+}
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "falsetruetrue");
+  });
 });
