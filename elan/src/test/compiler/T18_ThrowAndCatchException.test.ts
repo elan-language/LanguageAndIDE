856e376a070c0ccf83abdbf158e071d70446aa82
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-03-13 10:23:05 +0000

    #154 more tests

--- elan/src/test/compiler/T18_ThrowAndCatchException.test.ts
+++ elan/src/test/compiler/T18_ThrowAndCatchException.test.ts
@@ -182,4 +182,102 @@ export async function main() {
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeDoesNotExecute(fileImpl, "Foo");
   });
+
+  ignore_test('Pass_UseException', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  try
+    call foo()
+    print ""not caught""
+  catch e
+    print e.message
+  end try
+end main
+  
+procedure foo()
+  throw "Foo"
+end procedure`;
+
+    const objectCode = `var system : any; export function _inject(l : any) { system = l; };
+export async function main() {
+  try
+    foo()
+    print "not caught";
+  catch (_e) {
+    var e = _e as Error;
+    system.print(system.asString(e));
+  }
+}
+
+function foo() {
+  throw new Error("Foo");
+}
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeDoesNotExecute(fileImpl, "Foo");
+  });
+
+  ignore_test('Fail_ThrowExceptionInFunction', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var s set to foo("s")
+end main
+ 
+function foo(x String) as String
+  throw x
+  return x
+end function
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_catchMissingVariable', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  try
+    call foo()
+    print "not caught"
+  catch
+    print "caught"
+  end try
+end main
+  
+procedure foo()
+  throw "Foo"
+end procedure
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_UseExpressionForMessage', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var msg set to "Foo"
+  throw msg + bar
+end main
+`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
 });
