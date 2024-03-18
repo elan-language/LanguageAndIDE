3a991151fd450e12c5c070c2276be8647b3bac25
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-03-18 14:11:12 +0000

    #154 more tests

--- elan/src/test/compiler/T34_ConcreteClasses.test.ts
+++ elan/src/test/compiler/T34_ConcreteClasses.test.ts
@@ -42,11 +42,11 @@ class Foo {
     this.p1 = 5;
   }
 
-  p1 : number;
+  p1;
 
-  p2 : string;
+  p2;
 
-  asString() : string {
+  asString() {
     return "";
   }
 
@@ -62,11 +62,11 @@ class Foo {
     await assertObjectCodeExecutes(fileImpl, "123");
   });
 
-  ignore_test('Pass_Class_SimpleInstantiation_PropertyAccess_Methods', async () => {
+  ignore_test('Pass_ConstructorWithParm', async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
 main
-  var x set to new Foo(7, ""Apple"")
+  var x set to new Foo(7, "Apple")
   print x.p1
   print x.p2
 end main
@@ -80,29 +80,29 @@ class Foo
     property p1 Int
     property p2 String
     function asString() as String
-        return """"
+        return ""
     end function
 
 end class`;
 
     const objectCode = `var system; export function _inject(l) { system = l; };
 export async function main() {
-  var x = new Foo();
+  var x = new Foo(7, "Apple");
   system.print(system.asString(x.p1));
   system.print(system.asString(x.p2));
-  system.print(system.asString(x.asString()));
 }
 
 class Foo {
-  constructor() {
-    this.p1 = 5;
+  constructor(p_1 Int, p_2 String) {
+    this.p1 = p_1;
+    this.p2 = p_2;
   }
 
-  p1 : number;
+  p1;
 
-  p2 : string;
+  p2;
 
-  asString() : string {
+  asString() {
     return "";
   }
 
@@ -118,6 +118,201 @@ class Foo {
     await assertObjectCodeExecutes(fileImpl, "123");
   });
 
- 
+  test('Fail_NoConstructor', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+class Foo
+
+  property p1 Int
+  property p2 String
+  
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_InitialisePropertyInLine', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+class Foo
+
+    property p1 Int set to 3
+    property p2 String
+   
+    function asString() as String
+        return """"
+    end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_AttemptToModifyAPropertyDirectly', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to new Foo()
+  set x.p1 to 3
+end main
+
+class Foo
+  constructor()
+  end constructor
+
+  property p1 Int
+
+  function asString() as String
+      return """"
+  end function
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test('Fail_OverloadedConstructor', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+class Foo
+  constructor()
+  end constructor
+
+  constructor(val Int)
+      set p1 to val
+  end constructor
+
+  property p1 Int
+
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_InstantiateWithoutRequiredArgs', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to new Foo()
+end main
+
+class Foo
+  constructor(val Int)
+      set p1 to val
+  end constructor
+
+  property p1 Int
+
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_InstantiateWithWrongArgType', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to new Foo(7.1)
+end main
+
+class Foo
+  constructor(val Int)
+      set p1 to val
+  end constructor
+
+  property p1 Int
+
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_SupplyingArgumentNotSpecified', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to new Foo(7)
+end main
+
+class Foo
+  constructor()
+  end constructor
+
+  property p1 Int
+
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  ignore_test('Fail_MissingNewOnInstantiation', () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to Foo()
+  print x.p1
+  print x.p2
+  print x.asString()
+end main
+
+class Foo
+  constructor()
+      set p1 to 5
+  end constructor
+  property p1 Int
+
+  property p2 String
+
+  function asString() as String
+      return """"
+  end function
+
+end class`;
+
+    const fileImpl = new FileImpl(() => "", true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
 
 });
