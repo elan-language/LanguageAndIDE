ff843c4dbff1c845d2234d74cc1090b5b969d32f
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-04-15 16:26:48 +0100

    #264 more tests

--- elan/src/test/compiler/T35_Enumerations.test.ts
+++ elan/src/test/compiler/T35_Enumerations.test.ts
@@ -38,4 +38,72 @@ var Fruit = {
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeExecutes(fileImpl, "appleorangepear");
   });
+
+  test('Pass_useInVariable', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to Fruit.apple
+  set x to Fruit.pear
+  print x
+end main
+   
+enum Fruit
+  apple, orange, pear
+end enum`;
+
+    const objectCode = `var system; var _stdlib; export function _inject(l,s) { system = l; _stdlib = s; };
+export async function main() {
+  var x = Fruit.apple;
+  x = Fruit.pear;
+  system.print(_stdlib.asString(x));
+}
+
+var Fruit = {
+  apple : "apple", orange : "orange", pear : "pear"
+};
+`;
+
+    const fileImpl = new FileImpl(() => "", new DefaultProfile(), true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "pear");
+  });
+
+  test('Pass_useAsType', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to Fruit.apple
+  var y set to x
+  print y
+end main
+   
+enum Fruit
+  apple, orange, pear
+end enum`;
+
+    const objectCode = `var system; var _stdlib; export function _inject(l,s) { system = l; _stdlib = s; };
+export async function main() {
+  var x = Fruit.apple;
+  var y = x;
+  system.print(_stdlib.asString(y));
+}
+
+var Fruit = {
+  apple : "apple", orange : "orange", pear : "pear"
+};
+`;
+
+    const fileImpl = new FileImpl(() => "", new DefaultProfile(), true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "apple");
+  });
 });
