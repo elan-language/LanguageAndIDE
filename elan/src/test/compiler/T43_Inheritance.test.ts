c77d4428f1d8bd15e9d6638ecbb84008afc16671
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-04-18 13:40:28 +0100

    #264 start abstract class

---
+++ elan/src/test/compiler/T43_Inheritance.test.ts
@@ -0,0 +1,85 @@
+import { DefaultProfile } from "../../frames/default-profile";
+import { CodeSourceFromString, FileImpl } from "../../frames/file-impl";
+import { assertDoesNotParse, assertObjectCodeDoesNotExecute, assertObjectCodeExecutes, assertObjectCodeIs, assertParses, assertStatusIsValid, ignore_test } from "./compiler-test-helpers";
+import { createHash } from "node:crypto";
+
+suite('T43_Inheritance', () => {
+
+  ignore_test('Pass_DefineAbstractClassAndInheritFromIt', async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var x set to new Bar()
+  var l set to new List<of Foo>() + x
+  print x.p1
+  print x.p2
+  print x.product()
+  call x.setP1(4)
+  print x.product()
+end main
+
+abstract class Foo
+  abstract property p1 as Int
+  abstract property p2 as Int 
+  abstract procedure setP1(v as Int)
+  abstract function product() return Int
+end class
+
+class Bar inherits Foo
+    constructor()
+        set p1 to 3
+        set p2 to 4
+    end constructor
+    property p1 as Int
+    property p2 as Int
+
+    procedure setP1(p1 as Int)
+        set property.p1 to p1
+    end procedure
+
+    function product() return Int
+        return p1 * p2
+    end function
+
+    function asString() return String 
+        return ""
+    end function
+end class`;
+
+    const objectCode = `var system; var _stdlib; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var x = system.initialise(new Foo());
+  system.print(_stdlib.asString(x.p1));
+  system.print(_stdlib.asString(x.p2));
+  system.print(_stdlib.asString(x.asString()));
+}
+
+class Foo {
+  static defaultInstance() { return system.defaultClass(Foo, [["p1", "Int"], ["p2", "String"]]);};
+  constructor() {
+    this.p1 = 5;
+  }
+
+  p1 = 0;
+
+  p2 = "";
+
+  asString() {
+    return "";
+  }
+
+}
+return main;}`;
+
+    const fileImpl = new FileImpl(() => "", new DefaultProfile(), true);
+    fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "5");
+  });
+
+  
+
+});
