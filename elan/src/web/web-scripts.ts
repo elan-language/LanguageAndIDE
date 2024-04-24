c5fce43042f2e36bf7c7de76c2d6d7c56eb62720
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-04-24 14:21:31 +0100

    #330 hash function

--- elan/src/web/web-scripts.ts
+++ elan/src/web/web-scripts.ts
@@ -15,6 +15,17 @@ const codeFile = (<any>document.getElementsByClassName("elan-code")?.[0]).datase
 var doOnce = true;
 var profile : Profile;
 
+async function hash(toHash: string) {
+	const msgUint8 = new TextEncoder().encode(toHash); // encode as (utf-8) Uint8Array
+	const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+	const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+	const hashHex = hashArray
+		.map((b) => b.toString(16).padStart(2, "0"))
+		.join(""); // convert bytes to hex string
+	return hashHex;
+}
+
+
 fetch("profile.json", { mode: "same-origin" })
 	.then(f => f.json())
 	.then(j => {
