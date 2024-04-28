85b273506c907db020315efc0582aef3c06a5a92
Richard Pawson <rpawson@nakedobjects.net>
2024-04-28 13:48:43 +0100

    #340 delete old .json profile and add example Year 10

---
+++ elan/web-editor/year10-profile.js
@@ -0,0 +1,11 @@
+var localProfile = {
+   "name": "Year 10 Profile",
+
+   "globals":["main", "procedure", "function",  "constant", "test", "#"],
+   "statements":["assert", "call", "catch", "each", "else", "for", "if", "input", "print",
+                 "repeat", "return", "set", "var", "while", "#"],
+   "class_members": ["function", "procedure", "property", "abstract function", "abstract procedure", "abstract property","#"],
+
+   "include_profile_name_in_header": true,
+   "can_load_only_own_files": false
+};
