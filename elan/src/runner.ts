43598e215d9c179e62004c1d7d238e86b13dcea0
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-05-14 11:34:07 +0100

    #402 add test button still with bug

---
+++ elan/src/runner.ts
@@ -0,0 +1,18 @@
+import { System } from "./system";
+
+export function runTests(system : System, tests : [[string, () => void]]) {
+    system.print("Test Runner:");
+    for (const t of tests) {
+        try {
+            system.print(`\n${t[0]}:`);
+            t[1]();
+            system.print(` pass`);
+        }
+        catch (e) {
+            system.print(` fail`);
+            system.print(`- ${(e as any).message}`);
+        }
+    }
+
+    return system;
+}
