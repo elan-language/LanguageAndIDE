3f50dc630635f80c53525477fb507e1ec9f9dd19
Richard Pawson <rpawson@nakedobjects.net>
2024-05-14 10:33:50 +0100

    #411 fixed

--- elan/src/test/testHelpers.ts
+++ elan/src/test/testHelpers.ts
@@ -28,7 +28,7 @@ import { GenericParameterType } from '../symbols/generic-parameter-type';
 import { UnknownSymbol } from '../symbols/unknown-symbol';
 
 // flag to update test file 
-var updateTestFiles = true;
+var updateTestFiles = false;
 
 export async function assertEffectOfAction(sourceFile: string, action: (f: FileImpl) => void, htmlFile: string) {
   const ws = vscode.workspace.workspaceFolders![0].uri;
