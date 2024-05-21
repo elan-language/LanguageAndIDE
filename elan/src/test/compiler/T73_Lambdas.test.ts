608e3fc2cacca59d59e0e25621be265bc52cea5e
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-05-21 15:28:51 +0100

    #426 lambda work

--- elan/src/test/compiler/T73_Lambdas.test.ts
+++ elan/src/test/compiler/T73_Lambdas.test.ts
@@ -84,6 +84,75 @@ return [main, _tests];}`;
     await assertObjectCodeExecutes(fileImpl, "4");
   });
 
+
+  test("Pass_AssignALambdaToAVariable", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var l set to lambda x as Int => x * 5
+  print l(5)
+end main`;
+
+const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var l = (x) => x * 5;
+  system.print(_stdlib.asString(l(5)));
+}
+return [main, _tests];}`;
+  
+      const fileImpl = new FileImpl(
+        testHash,
+        new DefaultProfile(),
+        transforms(),
+        true,
+      );
+      await fileImpl.parseFrom(new CodeSourceFromString(code));
+  
+      assertParses(fileImpl);
+      assertStatusIsValid(fileImpl);
+      assertObjectCodeIs(fileImpl, objectCode);
+      await assertObjectCodeExecutes(fileImpl, "25");
+  });
+
+  test("Pass_ReturnALambda", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var l set to getFunc()
+  print l(5)
+end main
+    
+function getFunc() return Func<of Int => Int>
+  return lambda x as Int => x * 5
+end function`;
+
+    
+const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var l = getFunc();
+  system.print(_stdlib.asString(l(5)));
+}
+
+function getFunc() {
+  return (x) => x * 5;
+}
+return [main, _tests];}`;
+    
+        const fileImpl = new FileImpl(
+          testHash,
+          new DefaultProfile(),
+          transforms(),
+          true,
+        );
+        await fileImpl.parseFrom(new CodeSourceFromString(code));
+    
+        assertParses(fileImpl);
+        assertStatusIsValid(fileImpl);
+        assertObjectCodeIs(fileImpl, objectCode);
+        await assertObjectCodeExecutes(fileImpl, "25");
+  });
+
+
   test("Fail_PassLambdaWithWrongTypes", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
@@ -107,5 +176,26 @@ end procedure`;
     assertDoesNotCompile(fileImpl, ["Incompatible types String to Int"]);
   });
 
-  // Fails TODO
+  test("Fail_InvokeLambdaWithWrongType", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  call printModified("4", lambda x as Int => x + 3)
+end main
+
+procedure printModified(i as String, f as Func<of Int => Int>)
+  print f(i)
+end procedure`;
+
+    const fileImpl = new FileImpl(
+      testHash,
+      new DefaultProfile(),
+      transforms(),
+      true,
+    );
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertDoesNotCompile(fileImpl, ["Incompatible types String to Int"]);
+  });
 });
