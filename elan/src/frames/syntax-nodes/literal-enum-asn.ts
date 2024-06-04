20717ce0e1d7f6bf0e39b3c81df1fc8091a905f9
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-06-04 11:05:28 +0100

    #499 add compile errors for enums

--- elan/src/frames/syntax-nodes/literal-enum-asn.ts
+++ elan/src/frames/syntax-nodes/literal-enum-asn.ts
@@ -3,13 +3,16 @@ import { CompileError } from "../compile-error";
 import { Scope } from "../interfaces/scope";
 import { AbstractAstNode } from "./abstract-ast-node";
 import { AstNode } from "../interfaces/ast-node";
+import { transforms } from "./ast-helpers";
+import { mustBeKnownSymbol } from "../compile-rules";
+import { isScope } from "../helpers";
 
 export class LiteralEnumAsn extends AbstractAstNode implements AstNode {
   constructor(
     private readonly value: string,
     private readonly type: EnumType,
     public readonly fieldId: string,
-    scope: Scope,
+    private readonly scope: Scope,
   ) {
     super();
   }
@@ -20,6 +23,16 @@ export class LiteralEnumAsn extends AbstractAstNode implements AstNode {
 
   compile(): string {
     this.compileErrors = [];
+
+    const symbol = this.scope.resolveSymbol(this.type.name, transforms(), this.scope);
+
+    mustBeKnownSymbol(symbol, this.compileErrors, this.fieldId);
+
+    if (isScope(symbol)) {
+      const value = symbol.resolveSymbol(this.value, transforms(), this.scope);
+      mustBeKnownSymbol(value, this.compileErrors, this.fieldId);
+    }
+
     return `${this.type.name}.${this.value}`;
   }
 
