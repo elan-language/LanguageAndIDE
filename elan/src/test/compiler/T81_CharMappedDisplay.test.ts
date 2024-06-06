b46e057597a6e650ca1b6564fb3ec9720b889ebf
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-06-06 16:57:20 +0100

    #461 more tests

--- elan/src/test/compiler/T81_CharMappedDisplay.test.ts
+++ elan/src/test/compiler/T81_CharMappedDisplay.test.ts
@@ -81,6 +81,35 @@ return [main, _tests];}`;
     );
   });
 
+  test("Pass_GetAt", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var g set to getEmptyCharMap()
+  set g to g.putAt(10, 20, ("a", 1, 2))
+  print g.getAt(10, 20)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var g = _stdlib.getEmptyCharMap();
+  g = _stdlib.putAt(g, 10, 20, system.tuple(["a", 1, 2]));
+  system.print(_stdlib.asString(_stdlib.getAt(g, 10, 20)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "Tuple (a, 1, 2)");
+  });
+
   test("Pass_PutChar", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
@@ -112,6 +141,35 @@ return [main, _tests];}`;
       0,
       '<div style="foreground-color:0;background-color:0;">Z',
     );
+  });
+
+  test("Pass_GetChar", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var g set to getEmptyCharMap()
+  set g to g.putAt(10, 20, ("a", 1, 2))
+  print g.getChar(10, 20)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var g = _stdlib.getEmptyCharMap();
+  g = _stdlib.putAt(g, 10, 20, system.tuple(["a", 1, 2]));
+  system.print(_stdlib.asString(_stdlib.getChar(g, 10, 20)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "a");
   });
 
   test("Pass_PutForeground", async () => {
@@ -147,6 +205,35 @@ return [main, _tests];}`;
     );
   });
 
+  test("Pass_GetForeground", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var g set to getEmptyCharMap()
+  set g to g.putAt(10, 20, ("a", 1, 2))
+  print g.getForeground(10, 20)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var g = _stdlib.getEmptyCharMap();
+  g = _stdlib.putAt(g, 10, 20, system.tuple(["a", 1, 2]));
+  system.print(_stdlib.asString(_stdlib.getForeground(g, 10, 20)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "1");
+  });
+
   test("Pass_PutBackground", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
@@ -180,6 +267,35 @@ return [main, _tests];}`;
     );
   });
 
+  test("Pass_GetBackground", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var g set to getEmptyCharMap()
+  set g to g.putAt(10, 20, ("a", 1, 2))
+  print g.getBackground(10, 20)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var g = _stdlib.getEmptyCharMap();
+  g = _stdlib.putAt(g, 10, 20, system.tuple(["a", 1, 2]));
+  system.print(_stdlib.asString(_stdlib.getBackground(g, 10, 20)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "2");
+  });
+
   test("Pass_ClearGraphics", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
@@ -207,5 +323,38 @@ return [main, _tests];}`;
     assertStatusIsValid(fileImpl);
     assertObjectCodeIs(fileImpl, objectCode);
     await assertGraphicsContains(fileImpl, 0, "");
+  });
+
+  test("Pass_SetDefaultForeground", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  call setDefaultForeground(5)
+  var g set to getEmptyCharMap()
+  call g.drawCharMap()
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  _stdlib.setDefaultForeground(5);
+  var g = _stdlib.getEmptyCharMap();
+  _stdlib.drawCharMap(g);
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertGraphicsContains(
+      fileImpl,
+      0,
+      '<div style="foreground-color:5;background-color:0;">',
+    );
   });
 });
