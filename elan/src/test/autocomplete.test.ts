af16131b9174cea47b8b7fb4dde234352090ba41
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-06-25 15:08:01 +0100

    #573 more tests

--- elan/src/test/autocomplete.test.ts
+++ elan/src/test/autocomplete.test.ts
@@ -1,13 +1,11 @@
-import assert from "assert";
 import { CodeSourceFromString } from "../frames/code-source";
 import { DefaultProfile } from "../frames/default-profile";
-import { AssignableField } from "../frames/fields/assignableField";
 import { FileImpl } from "../frames/file-impl";
 import { testHash, transforms } from "./compiler/compiler-test-helpers";
 import { assertAutocompletes } from "./testHelpers";
 
 suite("Autocomplete", () => {
-  test("Pass_Minimal", async () => {
+  test("Pass_LocalVars", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
 main
@@ -25,5 +23,44 @@ end main`;
     ] as [string, string][];
 
     await assertAutocompletes(fileImpl, "ident10", expected);
+  });
+
+  test("Pass_FiltersByInput", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+main
+  var foo set to 1
+  var foobar set to 2
+  set foob
+end main`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    const expected = [["foobar", "Int"]] as [string, string][];
+
+    await assertAutocompletes(fileImpl, "ident10", expected);
+  });
+
+  test("Pass_NoConstant", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
+
+constant fooyon set to 3
+
+main
+  var foo set to 1
+  var foobar set to 2
+  set f
+end main`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    const expected = [
+      ["foo", "Int"],
+      ["foobar", "Int"],
+    ] as [string, string][];
+
+    await assertAutocompletes(fileImpl, "ident13", expected);
   });
 });
