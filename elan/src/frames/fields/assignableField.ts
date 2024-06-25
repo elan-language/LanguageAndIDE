357ab73506358b8c32b60322ccf675f40f711de4
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-06-25 14:50:57 +0100

    #573 first test

--- elan/src/frames/fields/assignableField.ts
+++ elan/src/frames/fields/assignableField.ts
@@ -7,7 +7,6 @@ import { AssignableNode } from "../parse-nodes/assignable-node";
 import { DeconstructedList } from "../parse-nodes/deconstructed-list";
 import { DeconstructedTuple } from "../parse-nodes/deconstructed-tuple";
 import { ParseNode } from "../parse-nodes/parse-node";
-import { VarStatement } from "../statements/var-statement";
 import { isVarStatement } from "../symbols/symbol-helpers";
 import { AbstractField } from "./abstract-field";
 
@@ -17,6 +16,10 @@ export class AssignableField extends AbstractField {
     this.setPlaceholder("variable");
     this.help = `A previously defined variable, but NOT a parameter. (For'tuple deconstruction' or 'list deconstruction' consult documentation.)`;
   }
+
+  // to make testing easier
+  autocompleteSymbols: ElanSymbol[] = [];
+
   getIdPrefix(): string {
     return "ident";
   }
@@ -39,8 +42,8 @@ export class AssignableField extends AbstractField {
   public textAsHtml(): string {
     let popupAsHtml = "";
     if (this.selected) {
-      const matchedSymbols = this.matchingSymbolsForId(this.getHolder());
-      const filteredSymbolIds = matchedSymbols.map((s) => s.symbolId);
+      this.autocompleteSymbols = this.matchingSymbolsForId(this.getHolder());
+      const filteredSymbolIds = this.autocompleteSymbols.map((s) => s.symbolId);
       popupAsHtml = this.popupAsHtml(filteredSymbolIds);
     }
     return super.textAsHtml() + popupAsHtml;
--- elan/src/test/autocomplete.test.ts
+++ elan/src/test/autocomplete.test.ts
@@ -1,18 +1,12 @@
+import assert from "assert";
 import { CodeSourceFromString } from "../frames/code-source";
 import { DefaultProfile } from "../frames/default-profile";
+import { AssignableField } from "../frames/fields/assignableField";
 import { FileImpl } from "../frames/file-impl";
-import {
-  testHash,
-  transforms,
-  assertParses,
-  assertStatusIsValid,
-  assertObjectCodeIs,
-  assertObjectCodeExecutes,
-  ignore_test,
-} from "./compiler/compiler-test-helpers";
+import { testHash, transforms } from "./compiler/compiler-test-helpers";
 
 suite("Autocomplete", () => {
-  ignore_test("Pass_Minimal", async () => {
+  test("Pass_Minimal", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan v0.1 valid
 
 main
@@ -23,5 +17,24 @@ end main`;
 
     const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
     await fileImpl.parseFrom(new CodeSourceFromString(code));
+    await fileImpl.renderAsHtml();
+
+    const fld = fileImpl.getById("ident10") as AssignableField;
+    const symbols = fld.autocompleteSymbols;
+
+    const expected = [
+      ["foo", "Int"],
+      ["foobar", "Int"],
+    ];
+
+    assert.strictEqual(symbols.length, expected.length);
+
+    for (let i = 0; i < expected.length; i++) {
+      const s = symbols[i];
+      const e = expected[i];
+
+      assert.strictEqual(s.symbolId, e[0]);
+      assert.strictEqual(s.symbolType(transforms()).name, e[1]);
+    }
   });
 });
