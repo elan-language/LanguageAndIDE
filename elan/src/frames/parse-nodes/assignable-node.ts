d366cb98687fd674f5d45656161534ed8f118ff8
Richard Pawson <rpawson@nakedobjects.net>
2024-06-27 13:09:13 +0100

    #592 proposed change to AssignableNode (but ASTVisitor needs fixing)

--- elan/src/frames/parse-nodes/assignable-node.ts
+++ elan/src/frames/parse-nodes/assignable-node.ts
@@ -1,24 +1,25 @@
 import { propertyKeyword } from "../keywords";
 import { AbstractSequence } from "./abstract-sequence";
+import { Alternatives } from "./alternatives";
 import { IdentifierNode } from "./identifier-node";
 import { IndexNode } from "./index-node";
 import { KeywordNode } from "./keyword-node";
 import { OptionalNode } from "./optional-node";
 import { Qualifier } from "./qualifier";
+import { Sequence } from "./sequence";
 
 export class AssignableNode extends AbstractSequence {
-  qualifier: OptionalNode;
-  simple: IdentifierNode;
+  simpleOrProp: Alternatives;
   index: OptionalNode;
 
   constructor() {
     super();
-    const qualDot = new Qualifier(new KeywordNode(propertyKeyword));
-    this.qualifier = new OptionalNode(qualDot);
-    this.simple = new IdentifierNode();
+    const propDot = () => new Qualifier(new KeywordNode(propertyKeyword));
+    const simple = () => new IdentifierNode();
+    const qualProp = () => new Sequence([propDot, simple]);
+    this.simpleOrProp = new Alternatives([simple, qualProp]);
+    this.addElement(this.simpleOrProp);
     this.index = new OptionalNode(new IndexNode());
-    this.addElement(this.qualifier);
-    this.addElement(this.simple);
     this.addElement(this.index);
   }
 }
