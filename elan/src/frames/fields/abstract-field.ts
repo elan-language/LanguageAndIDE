51f4098e8c5b03c57105ed1e888be58aff326599
Richard Pawson <rpawson@nakedobjects.net>
2024-06-27 12:06:06 +0100

    #573 fix sticky selected autocomplete

--- elan/src/frames/fields/abstract-field.ts
+++ elan/src/frames/fields/abstract-field.ts
@@ -510,6 +510,7 @@ export abstract class AbstractField implements Selectable, Field {
       symbolAsHtml.push(`<div class="autocomplete-item selected">${symbolIds[0]}</div>`);
       this.autoCompSelected = symbolIds[0];
     } else {
+      this.autoCompSelected = "";
       for (let i = startIndex; i < lastIndex; i++) {
         const symbolId = symbolIds[i];
         symbolAsHtml.push(
