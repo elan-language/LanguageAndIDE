f1b7238cd9d634c7e3fdc79bfcd187be28f44d86
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-07-03 14:47:25 +0100

    #604 more tests

---
+++ elan/src/test/compiler/T67_ListDeconstruction.test.ts
@@ -0,0 +1,15 @@
+import { DefaultProfile } from "../../frames/default-profile";
+import { CodeSourceFromString, FileImpl } from "../../frames/file-impl";
+import {
+  assertDoesNotCompile,
+  assertObjectCodeExecutes,
+  assertObjectCodeIs,
+  assertParses,
+  assertStatusIsValid,
+  testHash,
+  transforms,
+} from "./compiler-test-helpers";
+
+suite("T67_ListDeconstruction", () => {
+  // TODO
+});
