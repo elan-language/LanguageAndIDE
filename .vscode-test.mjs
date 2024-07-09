80615fe43be3410a6d0d1d38b828ecd5472d4eeb
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-07-09 13:52:36 +0100

    #641 use mocha to run tests directly

--- .vscode-test.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@vscode/test-cli';
-
-export default defineConfig({
-	files: 'out/test/**/*.test.js',
-	workspaceFolder: 'src/test/files',
-	mocha: {
-		timeout: 30000
-	  }
-});
