988a4c9f3a7cada2ad26a68fcd021ab7b270fddd
Richard Pawson <rpawson@nakedobjects.net>
2024-08-27 14:10:11 +0100

    #670 more rearranging existing options

--- src/frames/parse-nodes/term.ts
+++ src/frames/parse-nodes/term.ts
@@ -2,15 +2,14 @@ import { thisKeyword } from "../keywords";
 import { AbstractAlternatives } from "./abstract-alternatives";
 import { ArrayListNode } from "./array-list-node";
 import { BracketedExpression } from "./bracketed-expression";
-import { EmptyOfTypeNode } from "./empty-of-type-node";
+import { DictionaryNode } from "./dictionary-node";
 import { ExprNode } from "./expr-node";
 import { FunctionCallNode } from "./function-call-node";
-import { IfExpr } from "./if-expr";
+import { ImmutableDictionaryNode } from "./immutable-dictionary-node";
 import { ImmutableListNode } from "./immutable-list-node";
 import { KeywordNode } from "./keyword-node";
-import { Lambda } from "./lambda";
+import { LitValueNode } from "./lit-value";
 import { LiteralNode } from "./literal-node";
-import { NewInstance } from "./new-instance";
 import { TupleNode } from "./tuple-node";
 import { UnaryExpression } from "./unary-expression";
 import { VarRefNode } from "./var-ref-node";
@@ -22,16 +21,25 @@ export class Term extends AbstractAlternatives {
   }
 
   parseText(text: string): void {
-    //Sub nodes added only when asked to parse
-    this.alternatives.push(new BracketedExpression());
+    //Term simple:
+    //  SymbolNode:
+
+
+    this.alternatives.push(new ImmutableListNode(() => new ExprNode()));
+    this.alternatives.push(new ArrayListNode(() => new ExprNode()));
+    this.alternatives.push(new DictionaryNode(() => new ExprNode(), () => new ExprNode()));
+    this.alternatives.push(new ImmutableDictionaryNode(() => new ExprNode(), () => new ExprNode()));
+    this.alternatives.push(new TupleNode());
     this.alternatives.push(new UnaryExpression());
+    this.alternatives.push(new BracketedExpression());
+
+    this.alternatives.push(new LitValueNode());
     this.alternatives.push(new LiteralNode()); // Literal must be before Var to detect true/false
     this.alternatives.push(new KeywordNode(thisKeyword));
     this.alternatives.push(new VarRefNode());
     this.alternatives.push(new FunctionCallNode());
-    this.alternatives.push(new ImmutableListNode(() => new ExprNode()));
-    this.alternatives.push(new ArrayListNode(() => new ExprNode()));
-    this.alternatives.push(new TupleNode());
+
+
     super.parseText(text);
   }
 }
