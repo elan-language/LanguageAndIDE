4aa4144a7870c8d7e7339af175969744df2609e9
Richard Pawson <rpawson@nakedobjects.net>
2024-08-27 10:10:41 +0100

    #625 completed with more tests

--- test/compiler/StandardLib.test.ts
+++ test/compiler/StandardLib.test.ts
@@ -136,4 +136,55 @@ return [main, _tests];}`;
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeExecutes(fileImpl, "0, 0, 0, 3365, 3268, 3367, 0, ");
   });
+  test("passing Random type", async () => {
+    const code = `# FFFF Elan Beta 1 valid
+
+main
+  var results set to [0, 0, 0, 0, 0, 0, 0]
+  var rnd set to functionalRandomSeed(0, 0)
+  var dice set to 0
+  for i from 1 to 10000 step 1
+    set (dice, rnd) to rollDice(rnd)
+    set results[dice] to results[dice] + 1
+  end for
+  for i from 0 to 6 step 1
+    var r set to results[i]
+    print r
+    print ", "
+  end for
+end main
+
+function rollDice(rnd as Random) return (Int, Random)
+  return (rnd.valueInt(1, 6), rnd.next())
+end function`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var results = system.literalArray([0, 0, 0, 0, 0, 0, 0]);
+  var rnd = _stdlib.functionalRandomSeed(0, 0);
+  var dice = 0;
+  for (var i = 1; i <= 10000; i = i + 1) {
+    [dice, rnd] = rollDice(rnd);
+    system.safeSet(results, dice, system.safeIndex(results, dice) + 1);
+  }
+  for (var i = 0; i <= 6; i = i + 1) {
+    var r = system.safeIndex(results, i);
+    system.printLine(_stdlib.asString(r));
+    system.printLine(_stdlib.asString(", "));
+  }
+}
+
+function rollDice(rnd) {
+  return system.tuple([_stdlib.valueInt(rnd, 1, 6), _stdlib.next(rnd)]);
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "0, 1688, 1677, 1683, 1585, 1680, 1687, ");
+  });
 });
