7c7622733be00902c4653730473323b066f60b08
Richard Pawson <rpawson@nakedobjects.net>
2024-08-29 16:39:45 +0100

    #239 continued

--- src/frames/parse-nodes/lit-regex.ts
+++ src/frames/parse-nodes/lit-regex.ts
@@ -1,8 +1,14 @@
 import { Regexes } from "../fields/regexes";
-import { AbstractParseNode } from "./abstract-parse-node";
-import { matchRegEx } from "./parse-node-helpers";
+import { DIVIDE } from "../symbols";
+import { AbstractSequence } from "./abstract-sequence";
+import { RegExMatchNode } from "./regex-match-node";
+import { SymbolNode } from "./symbol-node";
 
-export class LitRegEx extends AbstractParseNode {
+export class LitRegEx extends AbstractSequence {
+  open: SymbolNode | undefined;
+  content: RegExMatchNode | undefined;
+  close: SymbolNode | undefined;
+
   constructor() {
     super();
     this.completionWhenEmpty = `"regular expression"`;
@@ -11,13 +17,20 @@ export class LitRegEx extends AbstractParseNode {
   parseText(text: string): void {
     this.remainingText = text;
     if (text.length > 0) {
-      [this.status, this.matchedText, this.remainingText] = matchRegEx(
-        text,
-        Regexes.regexExpression,
-      );
+      this.open = new SymbolNode(DIVIDE);
+      this.content = new RegExMatchNode(Regexes.anythingExceptUnescapedForwardSlash);
+      this.close = new SymbolNode(DIVIDE);
+      this.addElement(this.open);
+      this.addElement(this.content);
+      this.addElement(this.close);
+      super.parseText(text);
     }
   }
-  
+
+  renderAsSource(): string {
+    return `${this.open?.matchedText}${this.content?.matchedText}${this.close?.matchedText}`;
+  }
+
   renderAsHtml(): string {
     return `<regex>${this.renderAsSource()}</regex>`;
   }
