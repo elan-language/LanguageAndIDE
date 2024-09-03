ba6e50a1d21649466fb2d9f648a060a6504099e1
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-09-03 16:19:18 +0100

    #670 fix new index

--- src/frames/syntax-nodes/index-asn.ts
+++ src/frames/syntax-nodes/index-asn.ts
@@ -1,12 +1,24 @@
 import { CompileError } from "../compile-error";
+import {
+  mustBeIndexableSymbol,
+  mustBeCompatibleType,
+  mustBeRangeableSymbol,
+} from "../compile-rules";
 import { AstNode } from "../interfaces/ast-node";
 import { Scope } from "../interfaces/scope";
+import { SymbolType } from "../interfaces/symbol-type";
+import { ArrayListType } from "../symbols/array-list-type";
+import { FunctionType } from "../symbols/function-type";
+import { ImmutableListType } from "../symbols/immutable-list-type";
+import { IntType } from "../symbols/int-type";
+import { isGenericSymbolType, isDictionarySymbolType } from "../symbols/symbol-helpers";
 import { UnknownType } from "../symbols/unknown-type";
 import { AbstractAstNode } from "./abstract-ast-node";
+import { ChainedAsn } from "./chained-asn";
 import { ExprAsn } from "./expr-asn";
 import { RangeAsn } from "./range-asn";
 
-export class IndexAsn extends AbstractAstNode implements AstNode {
+export class IndexAsn extends AbstractAstNode implements AstNode, ChainedAsn {
   constructor(
     public readonly index1: ExprAsn,
     public readonly index2: ExprAsn | undefined,
@@ -16,16 +28,35 @@ export class IndexAsn extends AbstractAstNode implements AstNode {
     super();
   }
 
+  private precedingNode?: AstNode = undefined;
+  private updatedScope?: Scope = undefined;
+
+  updateScopeAndChain(s: Scope, ast: AstNode) {
+    this.updatedScope = s;
+    this.precedingNode = ast;
+  }
+
+  get showPreviousNode() {
+    return false;
+  }
+
   aggregateCompileErrors(): CompileError[] {
     return this.compileErrors.concat(this.index1.aggregateCompileErrors());
+  }
+
+  isRange() {
+    return this.index1 instanceof RangeAsn;
+  }
+
+  isIndex() {
+    return !(this.index1 instanceof RangeAsn);
   }
 
   isDoubleIndex() {
     return this.index2 !== undefined;
   }
 
-  compile(): string {
-    this.compileErrors = [];
+  compileIndexParameters() {
     if (this.index1 instanceof RangeAsn || this.index1 instanceof IndexAsn) {
       return `${this.index1.compile()}`;
     }
@@ -37,8 +68,109 @@ export class IndexAsn extends AbstractAstNode implements AstNode {
     return `${this.index1.compile()}`;
   }
 
+  wrapListOrArray(rootType: SymbolType, code: string): string {
+    if (rootType instanceof ImmutableListType) {
+      return `system.immutableList(${code})`;
+    }
+    if (rootType instanceof ArrayListType) {
+      return `system.array(${code})`;
+    }
+    if (rootType instanceof FunctionType) {
+      return this.wrapListOrArray(rootType.returnType, code);
+    }
+    return code;
+  }
+
+  wrapIndex(code: string): string {
+    if (this.isDoubleIndex()) {
+      return `system.safeDoubleIndex(${code})`;
+    }
+    return `system.safeIndex(${code})`;
+  }
+
+  getIndexType(rootType: SymbolType): [SymbolType, SymbolType] {
+    if (isGenericSymbolType(rootType)) {
+      return [IntType.Instance, rootType.ofType];
+    }
+    if (isDictionarySymbolType(rootType)) {
+      return [rootType.keyType, rootType.valueType];
+    }
+    return [UnknownType.Instance, UnknownType.Instance];
+  }
+
+  compileIndex(rootType: SymbolType, index: IndexAsn, q: string, idx: string) {
+    if (this.isDoubleIndex()) {
+      const [indexType, ofType] = this.getIndexType(rootType);
+
+      mustBeIndexableSymbol(ofType, true, this.compileErrors, this.fieldId);
+      mustBeCompatibleType(indexType, index.index1.symbolType(), this.compileErrors, this.fieldId);
+
+      const [indexType1] = this.getIndexType(ofType);
+
+      mustBeCompatibleType(
+        indexType1,
+        index.index2!.symbolType(),
+        this.compileErrors,
+        this.fieldId,
+      );
+    } else {
+      mustBeIndexableSymbol(rootType, true, this.compileErrors, this.fieldId);
+      const [indexType] = this.getIndexType(rootType);
+      mustBeCompatibleType(indexType, index.index1.symbolType(), this.compileErrors, this.fieldId);
+    }
+
+    let code = `${q}, ${idx}`;
+
+    code = this.wrapIndex(code);
+
+    return code;
+  }
+
+  compile(): string {
+    this.compileErrors = [];
+
+    if (!this.precedingNode) {
+      return this.compileIndexParameters();
+    }
+
+    const b = `${this.precedingNode!.compile()}`;
+    const idx = this.compileIndexParameters();
+    let code = `${b}`;
+
+    if (this.isIndex() || this.isRange()) {
+      // if (this.precedingNode) {
+      //   code = `${this.precedingNode.compile()}.${code}`;
+      // }
+
+      const rootType = this.precedingNode!.symbolType();
+
+      if (this.isIndex()) {
+        code = this.compileIndex(rootType, this, code, idx);
+      }
+      if (this.isRange()) {
+        mustBeRangeableSymbol(rootType, true, this.compileErrors, this.fieldId);
+        const [indexType] = this.getIndexType(rootType);
+        mustBeCompatibleType(indexType, IntType.Instance, this.compileErrors, this.fieldId);
+        code = `${code}${idx}`;
+        code = this.wrapListOrArray(rootType, code);
+      }
+    }
+
+    return code;
+  }
+
   symbolType() {
-    return UnknownType.Instance;
+    if (!this.precedingNode) {
+      return UnknownType.Instance;
+    }
+    const rootType = this.precedingNode.symbolType();
+
+    if (this.isIndex()) {
+      const [, ofType] = this.getIndexType(rootType);
+      return ofType;
+    }
+
+    return rootType;
   }
 
   toString() {
