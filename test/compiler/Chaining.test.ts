e723331ab054032b7ae5a442a878f3c3b6a5147b
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-09-03 11:15:41 +0100

    #670 more tests

--- test/compiler/Chaining.test.ts
+++ test/compiler/Chaining.test.ts
@@ -2,13 +2,12 @@ import { DefaultProfile } from "../../src/frames/default-profile";
 import { CodeSourceFromString, FileImpl } from "../../src/frames/file-impl";
 import {
   assertDoesNotCompile,
-    assertDoesNotCompileWithId,
-    assertObjectCodeExecutes,
-    assertObjectCodeIs,
-    assertParses,
-    assertStatusIsValid,
-    testHash,
-    transforms,
+  assertObjectCodeExecutes,
+  assertObjectCodeIs,
+  assertParses,
+  assertStatusIsValid,
+  testHash,
+  transforms
 } from "./compiler-test-helpers";
 
 suite("Chaining", () => {
@@ -25,6 +24,32 @@ end main`;
 async function main() {
   var a = system.immutableList([system.immutableList([1, 2]), system.immutableList([3, 4])]);
   var b = _stdlib.get(_stdlib.get(a, 1), 1);
+  system.printLine(_stdlib.asString(b));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "4");
+  });
+
+  test("Pass_ChainWithIndex", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan Beta 1 valid
+
+main 
+  var a set to {[1,2], [3,4]}
+  var b set to a.get(1)[1]
+  print b
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var a = system.immutableList([system.literalArray([1, 2]), system.literalArray([3, 4])]);
+  var b = system.safeIndex(_stdlib.get(a, 1), 1);
   system.printLine(_stdlib.asString(b));
 }
 return [main, _tests];}`;
