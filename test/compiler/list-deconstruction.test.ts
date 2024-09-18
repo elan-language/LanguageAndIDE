10aa19c8b07298fd09d382e6cbef6da70d7e792e
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-09-18 17:12:43 +0100

    #668 more test

--- test/compiler/list-deconstruction.test.ts
+++ test/compiler/list-deconstruction.test.ts
@@ -45,6 +45,39 @@ return [main, _tests];}`;
     await assertObjectCodeExecutes(fileImpl, "1[2, 3]");
   });
 
+  test("Pass_DeconstructExistingOneElement", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan Beta 2 valid
+
+main
+  var a set to [1]
+  var x set to 1
+  var y set to empty [Int]
+  set x:y to a
+  print x
+  print y
+end main
+`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var a = system.literalArray([1]);
+  var x = 1;
+  var y = system.emptyArray();
+  [x, y] = system.deconstructList(a);
+  system.printLine(_stdlib.asString(x));
+  system.printLine(_stdlib.asString(y));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "1[]");
+  });
+
   test("Pass_DeconstructIntoNewVariables", async () => {
     const code = `# FFFFFFFFFFFFFFFF Elan Beta 2 valid
 
@@ -76,5 +109,38 @@ return [main, _tests];}`;
     assertStatusIsValid(fileImpl);
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeExecutes(fileImpl, "1Int[2, 3][Int]");
+  });
+
+  test("Pass_DeconstructNewOneElement", async () => {
+    const code = `# FFFFFFFFFFFFFFFF Elan Beta 2 valid
+
+main
+  var a set to [1]
+  var x:y set to a
+  print x
+  print typeof x
+  print y
+  print typeof y
+end main
+`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var a = system.literalArray([1]);
+  var [x, y] = system.deconstructList(a);
+  system.printLine(_stdlib.asString(x));
+  system.printLine(_stdlib.asString("Int"));
+  system.printLine(_stdlib.asString(y));
+  system.printLine(_stdlib.asString("[Int]"));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "1Int[][Int]");
   });
 });
