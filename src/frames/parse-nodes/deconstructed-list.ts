02a2e92ff6b71dd7e1b61f0a5d3069ca92647a54
Richard Pawson <rpawson@nakedobjects.net>
2024-10-01 09:46:29 +0100

    #248 parse discard (underscore) in tuple- and list-deconstruction

--- src/frames/parse-nodes/deconstructed-list.ts
+++ src/frames/parse-nodes/deconstructed-list.ts
@@ -1,18 +1,22 @@
-import { COLON } from "../symbols";
+import { COLON, UNDERSCORE } from "../symbols";
 import { AbstractSequence } from "./abstract-sequence";
+import { Alternatives } from "./alternatives";
 import { IdentifierNode } from "./identifier-node";
 import { PunctuationNode } from "./punctuation-node";
 
 export class DeconstructedList extends AbstractSequence {
-  head: IdentifierNode | undefined;
-  tail: IdentifierNode | undefined;
+  head: Alternatives | undefined;
+  tail: Alternatives | undefined;
 
   parseText(text: string): void {
     if (text.length > 0) {
-      this.head = new IdentifierNode();
+      const head = () => new IdentifierNode();
+      const discard = () => new PunctuationNode(UNDERSCORE);
+      this.head = new Alternatives([head, discard]);
       this.addElement(this.head);
       this.addElement(new PunctuationNode(COLON));
-      this.tail = new IdentifierNode();
+      const tail = () => new IdentifierNode();
+      this.tail = new Alternatives([tail, discard]);
       this.addElement(this.tail);
       super.parseText(text);
     }
--- src/frames/parse-nodes/deconstructed-tuple.ts
+++ src/frames/parse-nodes/deconstructed-tuple.ts
@@ -1,13 +1,19 @@
+import { UNDERSCORE } from "../symbols";
 import { AbstractSequence } from "./abstract-sequence";
+import { Alternatives } from "./alternatives";
 import { CSV } from "./csv";
 import { IdentifierNode } from "./identifier-node";
+import { PunctuationNode } from "./punctuation-node";
 
 export class DeconstructedTuple extends AbstractSequence {
   csv: CSV | undefined;
 
   parseText(text: string): void {
     if (text.length > 0) {
-      this.csv = new CSV(() => new IdentifierNode(), 2);
+      const id = () => new IdentifierNode();
+      const discard = () => new PunctuationNode(UNDERSCORE);
+      const element = () => new Alternatives([id, discard]);
+      this.csv = new CSV(element, 2);
       this.addElement(this.csv);
       super.parseText(text);
     }
