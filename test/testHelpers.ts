a79a45709206c6295e707ab3c2e1fa6855f8ecac
Richard Pawson <rpawson@nakedobjects.net>
2024-10-02 10:26:16 +0100

    #751 fix broken test (and reset TestHelpers)

--- test/testHelpers.ts
+++ test/testHelpers.ts
@@ -22,7 +22,7 @@ import { assertParses, transforms } from "./compiler/compiler-test-helpers";
 import { getTestSystem } from "./compiler/test-system";
 
 // flag to update test file
-const updateTestFiles = true;
+const updateTestFiles = false;
 
 export async function assertEffectOfActionNew(
   sourceFile: string,
