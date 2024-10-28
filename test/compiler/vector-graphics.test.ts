bb90b3824d5bfa4881d671cc20b82ed20f7300b1
Richard Pawson <rpawson@nakedobjects.org>
2024-10-28 17:56:15 +0000

    #734 more tests

--- test/compiler/vector-graphics.test.ts
+++ test/compiler/vector-graphics.test.ts
@@ -31,14 +31,12 @@ end main`;
 
 main
   let vg be new VectorGraphics()
-  let circ be new CircleVG()
   print vg.asHtml()
 end main`;
 
     const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
 async function main() {
   const vg = system.initialise(new _stdlib.VectorGraphics());
-  const circ = system.initialise(new _stdlib.CircleVG());
   system.printLine(_stdlib.asString(vg.asHtml()));
 }
 return [main, _tests];}`;
@@ -96,6 +94,7 @@ return [main, _tests];}`;
 `,
     );
   });
+
   test("Pass_FullySpecifiedObject", async () => {
     const code = `# FFFF Elan Beta 3 valid
 
@@ -129,13 +128,139 @@ return [main, _tests];}`;
 `,
     );
   });
-  test("Pass_ReplaceObject", async () => {
+
+  test("Pass_RemoveLast", async () => {
     const code = `# FFFF Elan Beta 3 valid
 
 main
   let vg be new VectorGraphics()
   let circ be new CircleVG()
-  let vg2 be vg.add(circ)
+  let line be new LineVG()
+  let rect be new RectangleVG()
+  let vg2 be vg.add(circ).add(line).add(rect)
+  let vg3 be vg2.removeLast()
+  print vg3.asHtml()
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  const vg = system.initialise(new _stdlib.VectorGraphics());
+  const circ = system.initialise(new _stdlib.CircleVG());
+  const line = system.initialise(new _stdlib.LineVG());
+  const rect = system.initialise(new _stdlib.RectangleVG());
+  const vg2 = vg.add(circ).add(line).add(rect);
+  const vg3 = vg2.removeLast();
+  system.printLine(_stdlib.asString(vg3.asHtml()));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(
+      fileImpl,
+      `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="100%" cy="133.33333333333334%" r="11.25%" stroke="#000000" stroke-width="0.3%" fill="#ffff00" />
+  <line x1="0%" y1="0%" x2="100%" y2="133.33333333333334%" stroke="#000000" stroke-width="0.3%" />
+</svg>
+`,
+    );
+  });
+
+  test("Pass_Remove", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  let vg be new VectorGraphics()
+  let circ be new CircleVG()
+  let line be new LineVG()
+  let rect be new RectangleVG()
+  let vg2 be vg.add(circ).add(line).add(rect)
+  let vg3 be vg2.remove(line)
+  print vg3.asHtml()
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  const vg = system.initialise(new _stdlib.VectorGraphics());
+  const circ = system.initialise(new _stdlib.CircleVG());
+  const line = system.initialise(new _stdlib.LineVG());
+  const rect = system.initialise(new _stdlib.RectangleVG());
+  const vg2 = vg.add(circ).add(line).add(rect);
+  const vg3 = vg2.remove(line);
+  system.printLine(_stdlib.asString(vg3.asHtml()));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(
+      fileImpl,
+      `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+  <circle cx="100%" cy="133.33333333333334%" r="11.25%" stroke="#000000" stroke-width="0.3%" fill="#ffff00" />
+  <rect x="30%" y="53.333333333333336%" width="20%" height="13.333333333333334%" stroke="#000000" stroke-width="0.3%" fill="#0000ff" />
+</svg>
+`,
+    );
+  });
+
+  test("Pass_RemoveFirst", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  let vg be new VectorGraphics()
+  let circ be new CircleVG()
+  let line be new LineVG()
+  let rect be new RectangleVG()
+  let vg2 be vg.add(circ).add(line).add(rect)
+  let vg3 be vg2.remove(circ)
+  print vg3.asHtml()
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  const vg = system.initialise(new _stdlib.VectorGraphics());
+  const circ = system.initialise(new _stdlib.CircleVG());
+  const line = system.initialise(new _stdlib.LineVG());
+  const rect = system.initialise(new _stdlib.RectangleVG());
+  const vg2 = vg.add(circ).add(line).add(rect);
+  const vg3 = vg2.remove(circ);
+  system.printLine(_stdlib.asString(vg3.asHtml()));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(
+      fileImpl,
+      `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+  <line x1="0%" y1="0%" x2="100%" y2="133.33333333333334%" stroke="#000000" stroke-width="0.3%" />
+  <rect x="30%" y="53.333333333333336%" width="20%" height="13.333333333333334%" stroke="#000000" stroke-width="0.3%" fill="#0000ff" />
+</svg>
+`,
+    );
+  });
+
+  test("Pass_Replace", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  let vg be new VectorGraphics()
+  let circ be new CircleVG()
+  let line be new LineVG()
+  let rect be new RectangleVG()
+  let vg2 be vg.add(circ).add(line).add(rect)
   let circ2 be copy circ with fill to green
   let vg3 be vg2.replace(circ, circ2)
   print vg3.asHtml()
@@ -145,7 +270,9 @@ end main`;
 async function main() {
   const vg = system.initialise(new _stdlib.VectorGraphics());
   const circ = system.initialise(new _stdlib.CircleVG());
-  const vg2 = vg.add(circ);
+  const line = system.initialise(new _stdlib.LineVG());
+  const rect = system.initialise(new _stdlib.RectangleVG());
+  const vg2 = vg.add(circ).add(line).add(rect);
   const circ2 = (() => {const _a = {...circ}; Object.setPrototypeOf(_a, Object.getPrototypeOf(circ)); _a.fill = _stdlib.green; return _a;})();
   const vg3 = vg2.replace(circ, circ2);
   system.printLine(_stdlib.asString(vg3.asHtml()));
@@ -162,6 +289,8 @@ return [main, _tests];}`;
       fileImpl,
       `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
   <circle cx="100%" cy="133.33333333333334%" r="11.25%" stroke="#000000" stroke-width="0.3%" fill="#008000" />
+  <line x1="0%" y1="0%" x2="100%" y2="133.33333333333334%" stroke="#000000" stroke-width="0.3%" />
+  <rect x="30%" y="53.333333333333336%" width="20%" height="13.333333333333334%" stroke="#000000" stroke-width="0.3%" fill="#0000ff" />
 </svg>
 `,
     );
