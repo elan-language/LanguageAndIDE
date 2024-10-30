f3ed7ca4618573a62cd6c126fd5d68eb8c0859d0
Richard Pawson <rpawson@nakedobjects.org>
2024-10-30 09:50:31 +0000

    #793 more tests

--- test/compiler/set.test.ts
+++ test/compiler/set.test.ts
@@ -112,4 +112,104 @@ return [main, _tests];}`;
     assertObjectCodeIs(fileImpl, objectCode);
     await assertObjectCodeExecutes(fileImpl, "{4,3}");
   });
+  test("Pass_SetDifference", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  var st1 set to new Set<of Int>()
+  var st2 set to new Set<of Int>()
+  set st1 to st1.add(2).add(4).add(6).add(3)
+  set st2 to st2.add(3).add(1).add(4).add(9)
+  var st3 set to st1.difference(st2)
+  print st3
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  var st1 = system.initialise(new _stdlib.Set());
+  var st2 = system.initialise(new _stdlib.Set());
+  st1 = st1.add(2).add(4).add(6).add(3);
+  st2 = st2.add(3).add(1).add(4).add(9);
+  var st3 = st1.difference(st2);
+  system.printLine(_stdlib.asString(st3));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "{2,6}");
+  });
+  test("Pass_IsDisjointFrom", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  let st0 be new Set<of Int>()
+  let st1 be st0.add(2).add(4).add(6).add(3)
+  let st2 be st0.add(3).add(1).add(4).add(9)
+  let st3 be st0.add(8).add(9)
+  print st1.isDisjointFrom(st2)
+  print st1.isDisjointFrom(st3)
+  print st2.isDisjointFrom(st3)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  const st0 = system.initialise(new _stdlib.Set());
+  const st1 = st0.add(2).add(4).add(6).add(3);
+  const st2 = st0.add(3).add(1).add(4).add(9);
+  const st3 = st0.add(8).add(9);
+  system.printLine(_stdlib.asString(st1.isDisjointFrom(st2)));
+  system.printLine(_stdlib.asString(st1.isDisjointFrom(s3)));
+  system.printLine(_stdlib.asString(st2.isDisjointFrom(st3)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "falsetruefalse");
+  });
+  test("Pass_IsSubsetOfIsSupersetOf", async () => {
+    const code = `# FFFF Elan Beta 3 valid
+
+main
+  let st0 be new Set<of Int>()
+  let st1 be st0.add(2).add(4).add(6).add(3)
+  let st2 be sr0.add(4).add(6)
+  let st3 be st0.add(4).add(6).add(1)
+  print st2.isSubsetOf(st1)
+  print st2.isSupersetOf(st1)
+  print st1.isSupersetOf(st2)
+  print st1.isSupersetOf(st3)
+  print st3.isSupersetOf(st0)
+  print st0.isSubsetOf(st3)
+end main`;
+
+    const objectCode = `var system; var _stdlib; var _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+async function main() {
+  const st0 = system.initialise(new _stdlib.Set());
+  const st1 = st0.add(2).add(4).add(6).add(3);
+  const st2 = st0.add(3).add(1).add(4).add(9);
+  const st3 = st0..add(8).add(9);
+  system.printLine(_stdlib.asString(st1.isDisjointFrom(st2)));
+  system.printLine(_stdlib.asString(st1.isDisjointFrom(s3)));
+  system.printLine(_stdlib.asString(st2.isDisjointFrom(st3)));
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(fileImpl, "truefalsetruefalsetruetrue");
+  });
 });
