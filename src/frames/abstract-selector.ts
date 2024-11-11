712b8b824b141afeb09b465a3da8aba608db4bb9
Richard Pawson <rpawson@nakedobjects.org>
2024-11-11 16:59:54 +0000

    #819 <selector> with <el-select>

--- src/frames/abstract-selector.ts
+++ src/frames/abstract-selector.ts
@@ -125,7 +125,7 @@ export abstract class AbstractSelector extends AbstractFrame {
     this.text = "";
   }
   textToDisplayAsHtml(): string {
-    return `<selector><el-txt>${this.text}</el-txt><el-place>${this.label}</el-place><el-help class="selector">${this.getCompletion()}</el-help></selector>`;
+    return `<el-select><el-txt>${this.text}</el-txt><el-place>${this.label}</el-place><el-help class="selector">${this.getCompletion()}</el-help></el-select>`;
   }
   renderAsSource(): string {
     return `${this.indent()}`;
