5f6b027114eb9e0a187a21d298cfcade6a134e66
Richard Pawson <rpawson@nakedobjects.org>
2024-12-07 16:50:23 +0000

    #916

--- src/frames/fields/exception-message.ts
+++ src/frames/fields/exception-message.ts
@@ -1,19 +1,11 @@
 import { CodeSource } from "../code-source";
 import { Frame } from "../interfaces/frame";
-import { Alternatives } from "../parse-nodes/alternatives";
-import { IdentifierNode } from "../parse-nodes/identifier-node";
-import { LitString as LitStringNonEmpty } from "../parse-nodes/lit-string";
+import { ExceptionMsgNode } from "../parse-nodes/exception-msg-node";
 import { ParseNode } from "../parse-nodes/parse-node";
-import { TokenType } from "../symbol-completion-helpers";
 import { transforms } from "../syntax-nodes/ast-helpers";
 import { AbstractField } from "./abstract-field";
 
 export class ExceptionMessage extends AbstractField {
-  tokenTypes: Set<TokenType> = new Set<TokenType>([
-    TokenType.id_constant,
-    TokenType.id_let,
-    TokenType.id_variable,
-  ]);
   isParseByNodes = true;
   constructor(holder: Frame) {
     super(holder);
@@ -25,10 +17,7 @@ export class ExceptionMessage extends AbstractField {
   }
   initialiseRoot(): ParseNode {
     this.astNode = undefined;
-    this.rootNode = new Alternatives([
-      () => new LitStringNonEmpty(),
-      () => new IdentifierNode(this.tokenTypes),
-    ]);
+    this.rootNode = new ExceptionMsgNode();
     return this.rootNode;
   }
   readToDelimiter: (source: CodeSource) => string = (source: CodeSource) =>
