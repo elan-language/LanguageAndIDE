1583fd6cec811150ac872942009b7a020a6527f4
Richard Pawson <rpawson@nakedobjects.org>
2024-12-07 16:56:30 +0000

    #916

---
+++ src/frames/parse-nodes/var-def-node.ts
@@ -0,0 +1,13 @@
+import { AbstractAlternatives } from "./abstract-alternatives";
+import { DeconstructedList } from "./deconstructed-list";
+import { DeconstructedTuple } from "./deconstructed-tuple";
+import { IdentifierNode } from "./identifier-node";
+
+export class VarDefNode extends AbstractAlternatives {
+    constructor() {
+        super();
+        this.alternatives.push(new IdentifierNode());
+        this.alternatives.push(new DeconstructedTuple());
+        this.alternatives.push(new DeconstructedList());
+    }
+}
