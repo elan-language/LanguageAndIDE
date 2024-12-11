7047a0ac83f3b2a53b3dbe90a05cb7aed8482894
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-12-11 16:27:58 +0000

    #872

--- src/web/web-scripts.ts
+++ src/web/web-scripts.ts
@@ -194,13 +194,81 @@ for (const elem of demoFiles) {
   });
 }
 
-// fetch profile triggers page display
-fetchProfile()
-  .then(async (p) => await setup(p))
-  .catch(async (e) => {
-    console.warn("profile not found - using default");
-    await setup(new DefaultProfile());
-  });
+// from https://stackoverflow.com/questions/4565112/how-to-find-out-if-the-user-browser-is-chrome
+function checkIsChrome() {
+  // please note,
+  // that IE11 now returns undefined again for window.chrome
+  // and new Opera 30 outputs true for window.chrome
+  // but needs to check if window.opr is not undefined
+  // and new IE Edge outputs to true now for window.chrome
+  // and if not iOS Chrome check
+  // so use the below updated condition
+  const isChromium = (window as any).chrome;
+  const winNav = window.navigator;
+  const vendorName = (winNav as any).vendor;
+  const isOpera = typeof (window as any).opr !== "undefined";
+  const isFirefox = winNav.userAgent.indexOf("Firefox") > -1;
+  const isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+  const isIOSChrome = winNav.userAgent.match("CriOS");
+  const isGoogleChrome =
+    typeof (winNav as any).userAgentData !== "undefined"
+      ? (winNav as any).userAgentData.brands[0].brand === "Google Chrome"
+      : vendorName === "Google Inc.";
+
+  if (isIOSChrome) {
+    // is Google Chrome on IOS
+    return true;
+  } else if (
+    isChromium !== null &&
+    typeof isChromium !== "undefined" &&
+    vendorName === "Google Inc." &&
+    isOpera === false &&
+    isIEedge === false &&
+    isGoogleChrome
+  ) {
+    // is Google Chrome
+    return true;
+  } else {
+    // not Google Chrome
+    return false;
+  }
+}
+
+function confirmContinueOnNonChromeBrowser() {
+  return confirm("Not running chrome - ok to continue");
+}
+
+const isChrome = checkIsChrome();
+const okToContinue = isChrome || confirmContinueOnNonChromeBrowser();
+
+if (okToContinue) {
+  // fetch profile triggers page display
+  fetchProfile()
+    .then(async (p) => await setup(p))
+    .catch(async (e) => {
+      console.warn("profile not found - using default");
+      await setup(new DefaultProfile());
+    });
+} else {
+  const msg = "Require Chrome";
+  disable(runButton, msg);
+  disable(stopButton, msg);
+  disable(loadButton, msg);
+  disable(appendButton, msg);
+  disable(saveButton, msg);
+  disable(autoSaveButton, msg);
+  disable(newButton, msg);
+  disable(demosButton, msg);
+  disable(trimButton, msg);
+  disable(expandCollapseButton, msg);
+  disable(undoButton, msg);
+  disable(redoButton, msg);
+  disable(clearConsoleButton, msg);
+  disable(clearGraphicsButton, msg);
+  for (const elem of demoFiles) {
+    elem.setAttribute("hidden", "");
+  }
+}
 
 function checkForUnsavedChanges(): boolean {
   return hasUnsavedChanges()
