651a21382638a27d3968d41bd71329ab78b618ef
Stefano Cascarini <stef.cascarini@googlemail.com>
2024-12-17 15:56:29 +0000

    housekeeping - bump packages - npm i required

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/out", "**/dist", "**/*.d.ts", "**/*.html"],
+}, ...compat.extends("plugin:@typescript-eslint/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+
+    settings: {
+        "import/resolver": {
+            node: {
+                extensions: [".js", ".mjs", ".ts", ".d.ts"],
+            },
+
+            typescript: {
+                project: "./tsconfig.json",
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
+
+        "@/semi": "warn",
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        semi: "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-empty-object-type" : "off",
+        "@typescript-eslint/no-unused-expressions" : "off"
+    },
+}];
