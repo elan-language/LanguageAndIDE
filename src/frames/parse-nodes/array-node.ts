36fcc947f1310579964be7412b7ae1b39b2c587e
Richard Pawson <rpawson@nakedobjects.org>
2025-01-21 13:54:32 +0000

    #1050

--- src/frames/parse-nodes/array-node.ts
+++ src/frames/parse-nodes/array-node.ts
@@ -2,7 +2,9 @@ import { CLOSE_SQ_BRACKET, OPEN_SQ_BRACKET } from "../symbols";
 import { AbstractSequence } from "./abstract-sequence";
 import { CSV } from "./csv";
 import { ParseNode } from "./parse-node";
+import { Space } from "./parse-node-helpers";
 import { PunctuationNode } from "./punctuation-node";
+import { SpaceNode } from "./space-node";
 
 export class ArrayNode extends AbstractSequence {
   csv: CSV | undefined;
@@ -18,6 +20,7 @@ export class ArrayNode extends AbstractSequence {
       this.addElement(new PunctuationNode(OPEN_SQ_BRACKET));
       this.csv = new CSV(this.elementConstructor, 1);
       this.addElement(this.csv);
+      this.addElement(new SpaceNode(Space.ignored));
       this.addElement(new PunctuationNode(CLOSE_SQ_BRACKET));
       super.parseText(text);
     }
--- src/frames/parse-nodes/list-node.ts
+++ src/frames/parse-nodes/list-node.ts
@@ -2,7 +2,9 @@ import { CLOSE_BRACE, OPEN_BRACE } from "../symbols";
 import { AbstractSequence } from "./abstract-sequence";
 import { CSV } from "./csv";
 import { ParseNode } from "./parse-node";
+import { Space } from "./parse-node-helpers";
 import { PunctuationNode } from "./punctuation-node";
+import { SpaceNode } from "./space-node";
 
 export class ListNode extends AbstractSequence {
   csv: CSV | undefined;
@@ -18,6 +20,7 @@ export class ListNode extends AbstractSequence {
       this.addElement(new PunctuationNode(OPEN_BRACE));
       this.csv = new CSV(this.elementConstructor, 1);
       this.addElement(this.csv);
+      this.addElement(new SpaceNode(Space.ignored));
       this.addElement(new PunctuationNode(CLOSE_BRACE));
       super.parseText(text);
     }
