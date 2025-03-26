9d91dacd10c38d1f972eb4aaf27c900e8aa63aa4
Richard Pawson <rpawson@nakedobjects.org>
2025-03-26 11:30:44 +0000

    #1202 Make CircleVG mutable

--- src/standard-library/circle-vg.ts
+++ src/standard-library/circle-vg.ts
@@ -1,8 +1,17 @@
-import { ClassOption, ElanClass, elanClass, elanProperty } from "../elan-type-annotations";
-import { BaseVG } from "./base-vg";
+import {
+  ClassOption,
+  ElanClass,
+  elanClass,
+  elanFunction,
+  elanProcedure,
+  elanProperty,
+  FunctionOptions,
+} from "../elan-type-annotations";
+import { System } from "../system";
+import { VectorGraphic } from "./vector-graphic";
 
-@elanClass(ClassOption.record, [], [], [], [ElanClass(BaseVG)])
-export class CircleVG extends BaseVG {
+@elanClass(ClassOption.concrete, [], [], [], [ElanClass(VectorGraphic)])
+export class CircleVG extends VectorGraphic {
   static emptyInstance() {
     return new CircleVG();
   }
@@ -11,21 +20,59 @@ export class CircleVG extends BaseVG {
     return this;
   }
 
-  constructor() {
-    super();
-    this.cx = 100;
-    this.cy = 100;
-    this.r = 10;
+  private system?: System;
+
+  constructor(copy?: CircleVG) {
+    super(copy);
+    this.cx = copy ? copy.cx : 0;
+    this.cy = copy ? copy.cy : 0;
+    this.r = copy ? copy.r : 0;
   }
 
   @elanProperty()
   cx: number = 0;
 
+  @elanProcedure(["radius"])
+  setCx(cx: number) {
+    this.cx = cx;
+  }
+
+  @elanFunction(["cx"], FunctionOptions.pure, ElanClass(CircleVG))
+  withCx(cx: number): CircleVG {
+    const copy = this.system!.initialise(new CircleVG(this));
+    copy.cx = cx;
+    return copy;
+  }
+
   @elanProperty()
   cy: number = 0;
 
+  @elanProcedure(["radius"])
+  setCy(cy: number) {
+    this.cy = cy;
+  }
+
+  @elanFunction(["cy"], FunctionOptions.pure, ElanClass(CircleVG))
+  withCy(cy: number): CircleVG {
+    const copy = this.system!.initialise(new CircleVG(this));
+    copy.cy = cy;
+    return copy;
+  }
+
   @elanProperty()
   r: number = 0;
+
+  @elanProcedure(["radius"])
+  setRadius(r: number) {
+    this.r = r;
+  }
+
+  @elanFunction(["radius"], FunctionOptions.pure, ElanClass(CircleVG))
+  withRadius(r: number): CircleVG {
+    const copy = this.system!.initialise(new CircleVG(this));
+    copy.r = r;
+    return copy;
+  }
 
   asHtml(): string {
     return `<circle cx="${this.cx}%" cy="${this.cy / 0.75}%" r="${this.r * 1.125}%" stroke="${this.strokeAsColour()}" stroke-width="${this.strokeWidthPC()}%" fill="${this.fillAsColour()}" />`;
