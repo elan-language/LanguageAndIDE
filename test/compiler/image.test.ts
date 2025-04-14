7c31af966d157e8902328e1fbf39fd747a5df70e
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-04-14 10:53:32 +0100

    #1224 more tests

--- test/compiler/image.test.ts
+++ test/compiler/image.test.ts
@@ -3,6 +3,7 @@ import { DefaultProfile } from "../../src/frames/default-profile";
 import { FileImpl } from "../../src/frames/file-impl";
 import {
   assertDoesNotCompile,
+  assertDoesNotParse,
   assertObjectCodeExecutes,
   assertObjectCodeIs,
   assertParses,
@@ -39,5 +40,63 @@ return [main, _tests];}`;
       fileImpl,
       `<img src="http://website.image.png" width="0" height="0" title="">`,
     );
+  });
+
+  test("Pass_With", async () => {
+    const code = `${testHeader}
+
+main
+  variable c set to image http://website.image.png with width set to 100, height set to 200
+  print c
+end main`;
+
+    const objectCode = `let system; let _stdlib; let _tests = []; export function _inject(l,s) { system = l; _stdlib = s; }; export async function program() {
+const global = new class {};
+async function main() {
+  let c = await (async () => {const _a = {...system.initialise(await new _stdlib.Image()._initialise("http://website.image.png"))}; Object.setPrototypeOf(_a, Object.getPrototypeOf(system.initialise(await new _stdlib.Image()._initialise("http://website.image.png")))); _a.width = 100; _a.height = 200; return _a;})();
+  await system.printLine(c);
+}
+return [main, _tests];}`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), "", transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertObjectCodeIs(fileImpl, objectCode);
+    await assertObjectCodeExecutes(
+      fileImpl,
+      `<img src="http://website.image.png" width="100" height="200" title="">`,
+    );
+  });
+
+  test("Fail_invalidUrl", async () => {
+    const code = `${testHeader}
+
+main
+  variable c set to image website.image.png
+  print c
+end main`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), "", transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertDoesNotParse(fileImpl);
+  });
+
+  test("Fail_With", async () => {
+    const code = `${testHeader}
+
+main
+  variable c set to image http://website.image.png with z set to 100
+  print c
+end main`;
+
+    const fileImpl = new FileImpl(testHash, new DefaultProfile(), "", transforms(), true);
+    await fileImpl.parseFrom(new CodeSourceFromString(code));
+
+    assertParses(fileImpl);
+    assertStatusIsValid(fileImpl);
+    assertDoesNotCompile(fileImpl, ["'z' is not defined"]);
   });
 });
