ae41ae70d31faacd7346c511b4fb0eef4df2f9cc
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-04-22 16:13:19 +0100

    #457 test debugging

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -43,3 +43,41 @@ test('load and run demo', async ({ page }) => {
   await page.locator('#inp').press('Enter');
   await expect(page.locator('#printed-text')).toContainText("What type of fruit do you want ('x' to exit)? fig We can supply a fig What type of fruit do you want ('x' to exit)? ");
 });
+
+test('debug program', async ({ page }) => {
+  page.once('dialog', dialog => {
+    //console.log(`Dialog message: ${dialog.message()}`);
+    dialog.accept().catch(() => {});
+  });
+  await page.goto('https://elan-language.github.io/LanguageAndIDE/');
+
+  await page.getByText('main procedure function test').click();
+
+  await page.keyboard.type('p'); // procedure
+  await page.keyboard.type('foo');
+  await page.keyboard.press('Tab');
+  await page.keyboard.type('a as Int');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.type('p'); // print
+  await page.keyboard.type('100');
+  await page.keyboard.press('Enter');
+
+  await page.keyboard.press('ArrowLeft');
+  await page.keyboard.press('End'); // to outer selector
+
+  await page.keyboard.type('m'); // main
+  await page.keyboard.type('c'); // call
+  await page.keyboard.type('foo');
+  await page.keyboard.press('Tab');
+  await page.keyboard.type('100');
+
+  await page.getByText('print', { exact: true }).click({
+    button: 'right'
+  });
+  await page.getByText('set breakpoint (Ctrl-b)').click();
+  await page.getByRole('button', { name: 'Debug the program' }).click();
+  await expect(page.locator('#system-info')).toContainText('a : 100');
+  await expect(page.locator('#run-status')).toContainText('paused');
+ 
+});
