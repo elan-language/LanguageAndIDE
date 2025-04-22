7e46cb207b570330381270280a271e84c46ffe4e
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-04-22 15:21:03 +0100

    #457 another test

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -9,7 +9,7 @@ test('has title', async ({ page }) => {
 
 test('simple program', async ({ page }) => {
   page.once('dialog', dialog => {
-    console.log(`Dialog message: ${dialog.message()}`);
+    //console.log(`Dialog message: ${dialog.message()}`);
     dialog.accept().catch(() => {});
   });
   await page.goto('https://elan-language.github.io/LanguageAndIDE/');
@@ -24,4 +24,22 @@ test('simple program', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Run the program' }).click();
   await expect(page.locator('#printed-text')).toContainText('100');
+});
+
+test('load and run demo', async ({ page }) => {
+  page.once('dialog', dialog => {
+    //console.log(`Dialog message: ${dialog.message()}`);
+    dialog.accept().catch(() => {});
+  });
+  await page.goto('https://elan-language.github.io/LanguageAndIDE/');
+ 
+  await page.getByRole('button', { name: 'Demo' }).hover();
+  await page.getByText('Binary Search').click();
+
+  await page.getByRole('button', { name: 'Run the program' }).click();
+  await expect(page.locator('#printed-text')).toContainText("What type of fruit do you want ('x' to exit)? ");
+
+  await page.locator('#inp').fill('fig');
+  await page.locator('#inp').press('Enter');
+  await expect(page.locator('#printed-text')).toContainText("What type of fruit do you want ('x' to exit)? fig We can supply a fig What type of fruit do you want ('x' to exit)? ");
 });
