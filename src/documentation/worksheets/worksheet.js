5bf2bc913812de2cb7ea8762b3961f9dc6df9736
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-06-03 10:38:42 +0100

    #1366 more demo code

--- src/documentation/worksheets/worksheet.js
+++ src/documentation/worksheets/worksheet.js
@@ -1,3 +1,30 @@
+async function hash(toHash) {
+  const msgUint8 = new TextEncoder().encode(toHash); // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join(""); // convert bytes to hex string
+  return hashHex;
+}
+
+async function checkHash() {
+  const hash1 = document.getElementById("hash").innerHTML;
+
+  let code = new XMLSerializer().serializeToString(document);
+
+  const toReplace = `<div hidden="" id="hash">.*</div>`;
+  const re = new RegExp(toReplace);
+
+  code = code.replace(re, `<div hidden="" id="hash"></div>`);
+
+  const hash2 = await hash(code); 
+
+  if (hash1 !== hash2) {
+    alert("document has changed outside ide");
+  }
+}
+
+checkHash();
+
 const autoSaveButton = document.getElementById("auto-save");
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
@@ -6,7 +33,7 @@ const hint1 = document.getElementById("hint1");
 
 let fh = undefined;
 
-async function chromeSave(code, newName ) {
+async function chromeSave(code, newName) {
   const fh = await showSaveFilePicker({
     suggestedName: newName,
     startIn: "documents",
@@ -20,64 +47,101 @@ async function chromeSave(code, newName ) {
   return fh;
 }
 
-function getUpdatedDocument() {
-   let code = new XMLSerializer().serializeToString(document);
+async function getUpdatedDocument() {
+  let code = new XMLSerializer().serializeToString(document);
 
-    for (const e of document.getElementsByClassName("simple-input")) {
-      const id = e.id;
-      const v = e.value;
-      const toReplace = `id="${id}" value=".*"`;
-      const re = new RegExp(toReplace);
+  for (const e of document.getElementsByClassName("simple-input")) {
+    const id = e.id;
+    const v = e.value;
+    const toReplace = `id="${id}" value=".*"`;
+    const re = new RegExp(toReplace);
 
-      code = code.replace(re, `id="${id}" value="${v}"`);
-    }
+    code = code.replace(re, `id="${id}" value="${v}"`);
+  }
 
-    for (const e of document.getElementsByTagName("textarea")) {
-      const id = e.id;
-      const v = e.value;
-      const toReplace = `id="${id}">.*<`;
-      const re = new RegExp(toReplace);
+  for (const e of document.getElementsByTagName("textarea")) {
+    const id = e.id;
+    const v = e.value;
+    const toReplace = `id="${id}">.*<`;
+    const re = new RegExp(toReplace);
 
-      code = code.replace(re, `id="${id}">${v}<`);
-    }
+    code = code.replace(re, `id="${id}">${v}<`);
+  }
 
-     for (const e of document.getElementsByClassName("radio-input")) {
-      const id = e.id;
-      const v = e.checked ? "true" : "false";
-      const toReplace = `id="${id}" />`;
-      const re = new RegExp(toReplace);
+  for (const e of document.getElementsByClassName("radio-input")) {
+    const id = e.id;
+    const v = e.checked ? "true" : "false";
+    const toReplace = `id="${id}" />`;
+    const re = new RegExp(toReplace);
 
-      code = code.replace(re, `id="${id}" checked="${v}" />`);
-    }
+    code = code.replace(re, `id="${id}" checked="${v}" />`);
+  }
 
-    return code;
+  for (const e of document.getElementsByTagName("select")) {
+    const id = e.id;
+    const options = e.options;
+    const index = options.selectedIndex;
+
+    const v = options[index].value;
+
+    const toReplace = `value="${v}">`;
+    const re = new RegExp(toReplace);
+
+    code = code.replace(re, `value="${v}" selected>`);
+  }
+
+  const toReplace = `<div hidden="" id="hash">.*</div>`;
+  const re = new RegExp(toReplace);
+
+  code = code.replace(re, `<div hidden="" id="hash"></div>`);
+
+  const hashcode = await hash(code); 
+
+  code = code.replace(`<div hidden="" id="hash"></div>`, `<div hidden="" id="hash">${hashcode}</div>`);
+
+
+  return code;
 }
 
 
 autoSaveButton.addEventListener("click", async () => {
-    let code = getUpdatedDocument();
+  let code = await getUpdatedDocument();
 
-    fh = await chromeSave(code, "workSheet");
+  fh = await chromeSave(code, "workSheet");
 });
 
 async function save() {
-     if (fh) {
-      const code = getUpdatedDocument();
-      const writeable = await fh.createWritable();
-      await writeable.write(code);
-      await writeable.close();
-    }
+  if (fh) {
+    const code = await getUpdatedDocument();
+    const writeable = await fh.createWritable();
+    await writeable.write(code);
+    await writeable.close();
+  }
 }
 
 for (const e of document.getElementsByClassName("update")) {
   e.addEventListener("input", async (e) => {
+    const id = e.target.id;
+    const d = e.data ?? "todo"; 
+
+    const changelist = document.getElementById("changes");
+
+    const change = document.createElement("div");
+
+    const changeText = document.createTextNode(`${id}:${d}`);
+ 
+    change.appendChild(changeText);
+
+    changelist.appendChild(change);
+
+
     await save();
   });
 }
 
 hint1.addEventListener("click", async () => {
-    const text = document.getElementById("hint1-text").innerHTML;
-    hint1.innerHTML = atob(text);
-    document.title = "Hint1 shown";
-    await save();
+  const text = document.getElementById("hint1-text").innerHTML;
+  hint1.innerHTML = atob(text);
+  document.title = "Hint1 shown";
+  await save();
 });
