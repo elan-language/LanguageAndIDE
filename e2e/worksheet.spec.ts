c6c3397945137b6240373329a0dab9843de2ac27
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-06-17 15:16:04 +0100

    Update worksheet.spec.ts

--- e2e/worksheet.spec.ts
+++ e2e/worksheet.spec.ts
@@ -5,6 +5,11 @@ test('load and run worksheet', async ({ page }) => {
     //console.log(`Dialog message: ${dialog.message()}`);
     dialog.accept().catch(() => {});
   });
+
+  function workSheetFrame() {
+    return page.frameLocator('#worksheet-iframe');
+  }
+
   await page.goto('https://elan-language.github.io/LanguageAndIDE/');
 
   await page.getByText('Worksheet', { exact: true }).click();
@@ -16,9 +21,31 @@ test('load and run worksheet', async ({ page }) => {
   // to move off dropdown
   await page.getByRole('button', { name: 'Load external worksheet' }).hover();
 
-  await expect(page.frameLocator('#worksheet-iframe').getByRole('button', { name: 'Auto-save to file' })).toBeVisible();
+  await expect(workSheetFrame().getByRole('button', { name: 'Auto-save to file' })).toBeVisible();
 
-  await page.frameLocator('#worksheet-iframe').getByRole('button', { name: 'Auto-save to file' }).click();
+  await workSheetFrame().getByRole('button', { name: 'Auto-save to file' }).click();
 
-  await expect(page.frameLocator('#worksheet-iframe').getByRole('checkbox', {name : 'Preliminaries completed'})).toBeVisible();
+  await expect(workSheetFrame().getByRole('checkbox', {name : 'Preliminaries completed'})).toBeVisible();
+
+  await workSheetFrame().getByRole('checkbox', {name : 'Preliminaries completed'}).click();
+
+  await expect(workSheetFrame().getByText('All required inputs must be completed to continue')).toBeVisible();
+
+  await workSheetFrame().getByRole('textbox').fill("a user");
+
+  await workSheetFrame().getByRole('checkbox', {name : 'Preliminaries completed'}).click();
+
+  await expect(workSheetFrame().getByText('All required inputs must be completed to continue')).not.toBeVisible();
+
+  await expect(workSheetFrame().getByText('Step 1')).toBeVisible();
+
+  await expect(workSheetFrame().getByText('Hint: Title')).toBeVisible();
+
+  await expect(workSheetFrame().getByText('Total hints used: 0/1')).toBeVisible();
+
+  await workSheetFrame().getByText('Hint: Title').click();
+
+  await expect(workSheetFrame().getByText('Total hints used: 1/1')).toBeVisible();
+
+  await expect(workSheetFrame().locator('.timestamp')).toHaveCount(2);
 });
