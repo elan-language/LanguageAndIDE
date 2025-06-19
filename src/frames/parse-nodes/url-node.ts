cb0344f4d2708c31e932caf2e035b8fabca5c987
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-06-19 12:19:52 +0100

    #1491 tweak

--- src/frames/parse-nodes/url-node.ts
+++ src/frames/parse-nodes/url-node.ts
@@ -16,8 +16,11 @@ export class UrlNode extends AbstractParseNode {
 
   validateUrl(text: string) {
     try {
-      const url = new URL(text);
-      this.set(ParseStatus.valid, text, "");
+      const url = text.split(" ");
+      const remainingText = url.length === 1 ? "" : url.slice(1).join("");
+
+      new URL(url[0]);
+      this.set(ParseStatus.valid, text, remainingText);
     } catch {
       this.set(ParseStatus.invalid, text, "");
     }
@@ -26,35 +29,25 @@ export class UrlNode extends AbstractParseNode {
   parseText(text: string): void {
     this.remainingText = text;
     if (text.length > 0) {
-      switch (text.length) {
-        case 1:
-          this.validate(["h"], text);
-          return;
-        case 2:
-          this.validate(["ht"], text);
-          return;
-        case 3:
-          this.validate(["htt"], text);
-          return;
-        case 4:
-          this.validate(["http"], text);
-          return;
-        case 5:
-          this.validate(["https", "http:"], text);
-          return;
-        case 6:
-          this.validate(["https:", "http:/"], text);
-          return;
-        case 7:
-          this.validate(["https:/", "http://"], text);
-          return;
-        default: {
-          if (text.startsWith("https://") || text.startsWith("http://")) {
-            this.validateUrl(text);
-          } else {
-            this.set(ParseStatus.invalid, text, "");
-          }
-          return;
+      if (text.length <= 7) {
+        if ("https:/".startsWith(text) || "http://".startsWith(text)) {
+          this.set(ParseStatus.incomplete, text, "");
+        } else {
+          this.set(ParseStatus.invalid, text, "");
+        }
+      } else if (text.length === 8) {
+        if (text.startsWith("https://")) {
+          this.set(ParseStatus.incomplete, text, "");
+        } else if (text.startsWith("http://")) {
+          this.validateUrl(text);
+        } else {
+          this.set(ParseStatus.invalid, text, "");
+        }
+      } else {
+        if (text.startsWith("https://") || text.startsWith("http://")) {
+          this.validateUrl(text);
+        } else {
+          this.set(ParseStatus.invalid, text, "");
         }
       }
     }
