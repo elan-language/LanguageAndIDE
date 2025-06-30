9c55e4903a27079ccba651d481842dfe8e3c9f70
Richard Pawson <rpawson@nakedobjects.org>
2025-06-30 10:25:59 +0100

    Move both Dev & Prod up to v1.2.5

--- test/testHelpers.ts
+++ test/testHelpers.ts
@@ -35,7 +35,7 @@ import { getTestRunner } from "./runner";
 
 
 // flag to update test file
-const updateTestFiles = true;
+const updateTestFiles = false;
 
 export async function assertEffectOfActionNew(
   sourceFile: string,
