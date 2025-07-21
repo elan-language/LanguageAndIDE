58873f821f440dfcd15e3732b0cd9725bb6c59b5
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-07-21 16:05:01 +0100

    #1688 deconstructed tuple

--- src/frames/syntax-nodes/frame-asn.ts
+++ src/frames/syntax-nodes/frame-asn.ts
@@ -6,6 +6,7 @@ import { SymbolType } from "../compiler-interfaces/symbol-type";
 import { singleIndent } from "../frame-helpers";
 import { BreakpointEvent, BreakpointStatus } from "../status-enums";
 import { ClassType } from "../symbols/class-type";
+import { DeconstructedTupleType } from "../symbols/deconstructed-tuple-type";
 import { EnumType } from "../symbols/enum-type";
 import { allScopedSymbols, getGlobalScope, orderSymbol } from "../symbols/symbol-helpers";
 import { SymbolScope } from "../symbols/symbol-scope";
@@ -132,6 +133,17 @@ export class FrameAsn extends AbstractAstNode implements AstNode, Scope {
 
       for (const s of ofTypes) {
         typeDict["OfTypes"].push(this.getClassTypeMap(s));
+      }
+
+      return typeDict;
+    } else if (type instanceof DeconstructedTupleType) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const typeDict: { [index: string]: any } = { Type: "DeconstructedTuple" };
+
+      typeDict["Ids"] = {};
+
+      for (const id of type.ids) {
+        typeDict["Ids"][id] = this.getClassTypeMap(type.symbolTypeFor(id));
       }
 
       return typeDict;
--- src/web/web-scripts.ts
+++ src/web/web-scripts.ts
@@ -1835,6 +1835,8 @@ function getDebugSymbolHtml(name: string, value: any, typeMap: { [index: string]
       return getDebugSymbolArray2D(name, value, typeMap);
     case "tuple":
       return getDebugSymbolTuple(name, value, typeMap);
+    case "DeconstructedTuple":
+      return getDebugSymbolHtml(name, value, typeMap["Ids"][name]);
     default:
       return getDebugSymbolClass(name, value, typeMap);
   }
