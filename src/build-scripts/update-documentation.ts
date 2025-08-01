e58c8d993d6d19ef15bc609be2a3688cc6dae69d
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-08-01 12:08:41 +0100

    #1741 fix paths in website documentation

---
+++ src/build-scripts/update-documentation.ts
@@ -0,0 +1,36 @@
+import { readdirSync, readFileSync, writeFileSync } from "node:fs";
+
+const rootdir = `${__dirname}/../../..`;
+
+const docs = `${rootdir}/out/website/documentation/`;
+
+function loadFile(fileName: string): string {
+  return readFileSync(fileName, "utf-8");
+}
+
+function saveFile(fileName: string, newContent: string) {
+  writeFileSync(fileName, newContent);
+}
+
+function getDocs(sourceDir: string): string[] {
+  return readdirSync(sourceDir).filter((s) => s.endsWith(".html"));
+}
+
+function updatePaths(contents: string) {
+  const repoUrl = `<link href="../ide/styles/`;
+  const prodUrl = `<link href="../styles/`;
+  const from = repoUrl;
+  const to = prodUrl;
+  const newContents = contents.replaceAll(from, to);
+  return newContents;
+}
+
+function updateFile(fileName: string) {
+  const contents = loadFile(fileName);
+  const newContents = updatePaths(contents);
+  saveFile(fileName, newContents);
+}
+
+for (const fn of getDocs(docs)) {
+  updateFile(`${docs}${fn}`);
+}
