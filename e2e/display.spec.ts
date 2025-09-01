2e2624be859c66337b05fe3961638ce954e1d799
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-09-01 10:25:52 +0100

    #1778 test

--- e2e/display.spec.ts
+++ e2e/display.spec.ts
@@ -19,3 +19,38 @@ test('display html', async ({ page }) => {
   await page.getByRole('button', { name: 'run' }).click();
   await expect(page.frameLocator('#display-html-sandbox').locator('#test-value')).toContainText('fred');
 });
+
+test('clear buffer', async ({ page }) => {
+  page.once('dialog', dialog => {
+    //console.log(`Dialog message: ${dialog.message()}`);
+    dialog.accept().catch(() => {});
+  });
+  await page.goto('https://elan-language.github.io/LanguageAndIDE/');
+ 
+  await page.getByText('main procedure function test').click();
+
+  await page.keyboard.type('m');
+  await page.keyboard.type('l');
+  await page.keyboard.type('a');
+  await page.keyboard.press('Tab');
+  await page.keyboard.type('inputInt("")');
+  await page.keyboard.press('Enter');
+  await page.keyboard.type('l');
+  await page.keyboard.type('b');
+  await page.keyboard.press('Tab');
+  await page.keyboard.type('waitForKey()');
+  await page.keyboard.press('Enter');
+  await page.keyboard.type('p');
+  await page.keyboard.type('b');
+
+
+  await page.getByRole('button', { name: 'run' }).click();
+
+  await page.locator('#inp').fill('2');
+  await page.locator('#inp').press('Enter');
+  await expect(page.locator('#printed-text')).toContainText("2");
+  await page.keyboard.press('Enter');
+  await expect(page.locator('#printed-text')).toContainText("Enter");
+
+
+});
