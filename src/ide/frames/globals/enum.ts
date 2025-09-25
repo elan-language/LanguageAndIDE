7f4b0e187fbdffee442f6d00ac2281653ca48534
Richard Pawson <rpawson@nakedobjects.org>
2025-09-25 12:09:18 +0100

    #835 don't render children of imported frames in Html

--- src/ide/frames/globals/enum.ts
+++ src/ide/frames/globals/enum.ts
@@ -38,7 +38,7 @@ export class Enum extends AbstractFrame implements GlobalFrame, Collapsible {
     return "enum";
   }
   renderAsHtml(): string {
-    return `<el-enum class="${this.cls()}" id='${this.htmlId}' tabindex="-1" ${this.toolTip()}><el-top>${this.contextMenu()}${this.bpAsHtml()}<el-expand>+</el-expand><el-kw>enum </el-kw>${this.name.renderAsHtml()}</el-top> ${this.values.renderAsHtml()}${this.helpAsHtml()}${this.compileMsgAsHtml()}${this.getFrNo()}</el-enum>`;
+    return `<el-enum class="${this.cls()}" id='${this.htmlId}' tabindex="-1" ${this.toolTip()}><el-top>${this.contextMenu()}${this.bpAsHtml()}<el-expand>+</el-expand><el-kw>enum </el-kw>${this.name.renderAsHtml()}</el-top> ${this.isImported() ? "" : this.values.renderAsHtml()}${this.helpAsHtml()}${this.compileMsgAsHtml()}${this.getFrNo()}</el-enum>`;
   }
 
   indent(): string {
