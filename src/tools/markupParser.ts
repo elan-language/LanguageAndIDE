4444b33a4eaf8dcd43e5d0d8b16cf96cf525dee8
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-09-30 11:47:11 +0100

    #1601 add hint processing

--- src/tools/markupParser.ts
+++ src/tools/markupParser.ts
@@ -1,6 +1,6 @@
 import { JSDOM } from "jsdom";
 
-export async function processStepJsDom(markup: string, instance: number): Promise<string> {
+export async function processStep(markup: string, instance: number): Promise<string> {
   const input = new JSDOM(markup);
   const inDoc = input.window.document;
 
@@ -11,7 +11,9 @@ export async function processStepJsDom(markup: string, instance: number): Promis
 
   const div = outDoc.querySelector("div")!;
 
-  div.innerHTML = step?.innerHTML ?? "";
+  const updatedHints = await processHints(step?.innerHTML ?? "");
+
+  div.innerHTML = updatedHints;
 
   const label = outDoc.createElement("label");
   label.className = "done";
@@ -43,6 +45,54 @@ export async function processStepJsDom(markup: string, instance: number): Promis
   return stepHtml;
 }
 
+export async function processHint(markup: string, instance: number): Promise<string> {
+  const input = new JSDOM(markup);
+  const inDoc = input.window.document;
+
+  const output = new JSDOM(`<div class="hint" id="hint${instance}" data-hint=""></div>`);
+  const outDoc = output.window.document;
+
+  const hint = inDoc.querySelector("hint");
+
+  const div = outDoc.querySelector("div")!;
+
+  div.textContent = hint?.textContent ?? "";
+
+  const content = hint?.nextElementSibling;
+
+  if (content) {
+    const contentAsString = content.innerHTML;
+
+    const encoded = btoa(contentAsString);
+
+    hint.setAttribute("data-hint", encoded);
+  }
+
+  const hintHtml = div.outerHTML;
+
+  return hintHtml;
+}
+
+async function processEachHintInstance(
+  initialCode: string,
+  startAt: number,
+  instance: number,
+): Promise<[string, number, number]> {
+  const toProcessCode = initialCode.slice(startAt);
+
+  const codeStart = toProcessCode.indexOf("<hint");
+  const codeEnd = toProcessCode.indexOf("</content>");
+
+  if (codeStart !== -1) {
+    const code = toProcessCode.slice(codeStart, codeEnd);
+    const htmlCode = await processHint(code, instance);
+
+    return [htmlCode, startAt + codeStart, startAt + codeEnd + 10];
+  }
+
+  return ["", -1, -1];
+}
+
 async function processEachStepInstance(
   initialCode: string,
   startAt: number,
@@ -55,12 +105,38 @@ async function processEachStepInstance(
 
   if (codeStart !== -1) {
     const code = toProcessCode.slice(codeStart, codeEnd);
-    const htmlCode = await processStepJsDom(code, instance);
+    const htmlCode = await processStep(code, instance);
 
     return [htmlCode, startAt + codeStart, startAt + codeEnd + 7];
   }
 
   return ["", -1, -1];
+}
+
+export async function processHints(source: string) {
+  const updates: [string, number, number][] = [];
+  let hintInstance = 0;
+
+  let [updatedCode, codeStart, codeEnd] = await processEachHintInstance(source, 0, hintInstance);
+  hintInstance++;
+  while (updatedCode !== "") {
+    updates.push([updatedCode, codeStart, codeEnd]);
+    [updatedCode, codeStart, codeEnd] = await processEachHintInstance(
+      source,
+      codeEnd,
+      hintInstance,
+    );
+    hintInstance++;
+  }
+
+  let updatedContent = source;
+
+  for (let i = updates.length - 1; i >= 0; i--) {
+    const [c, s, e] = updates[i];
+    updatedContent = updatedContent.slice(0, s) + c + updatedContent.slice(e);
+  }
+
+  return updatedContent;
 }
 
 export async function processSteps(source: string) {
