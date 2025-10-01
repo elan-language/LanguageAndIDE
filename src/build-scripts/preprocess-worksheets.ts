6962a4036be8be17a56a0779894ccae0dbc4e1a9
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-10-01 15:40:58 +0100

    #1601 boilerplate

--- src/build-scripts/preprocess-worksheets.ts
+++ src/build-scripts/preprocess-worksheets.ts
@@ -19,11 +19,70 @@ function getWorksheets(sourceDir: string): string[] {
   return readdirSync(sourceDir).filter((s) => s.endsWith(".html"));
 }
 
+function wrapInWorkSheetBoilerPlate(content: string) {
+  return `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en-GB">
+
+  <head>
+      <meta http-equiv="Cache-Control" content="no-store" />
+      <link href="../../../out/website/styles/colourScheme.css" rel="stylesheet" />
+      <link href="../../../out/website/styles/elanStyle.css" rel="stylesheet" />
+      <link href="../../../out/website/styles/documentation.css" rel="stylesheet" />
+      <link href="../../../out/website/styles/worksheet.css" rel="stylesheet" />
+      <title>Worksheet</title>
+  </head>
+
+  <body>
+    <div id="worksheet">
+    <div class="docTitle">Title</div>
+    <button id="auto-save">Auto-save to file</button><span> to continue. (After that any entries made into the worksheet will be automatically saved, and you can re-load the partially-completed worksheet in future &ndash; at which point you will be asked to auto-save it again).</span>
+    ${content}
+    <script src="../../../out/website/worksheet-scripts.js"></script>
+  </body>
+</html>`;
+}
+
+function prependFirstStep(content: string) {
+  return `<step>
+  <h3>Preliminaries</h3>
+  <label for="username">Enter your name: </label>
+  <input class="question" type="text" id="username" />
+  <ul>
+    <li>Set the browser to Full Screen view, to give this worksheet, and your code, as much space as possible.</li>
+  </ul>
+</step>
+${content}`;
+}
+
+function appendLastStep(content: string) {
+  return `${content}
+<step>
+  <p>Congratulations! You have finished the worksheet, and you now know enough about navigating and editing
+  code to start writing your first program &ndash; with the help of another Worksheet. Meantime,
+  click <b>Help > Home > IDE Reference > Code editor</b> &ndash; for a handy summary of all the above
+  operations, and a few more that you can also try out here.</p>
+  <div class="license">
+      <img decoding="async" loading="lazy" src="https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-nc-nd.png" width="118" height="41">
+      This worksheet is copyright &copy; Richard Pawson 2025, and protected by the Creative Commons license:
+          <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank"> Attribution-NonCommercial-NoDerivatives 4.0 International</a>.
+        If you copy and <i>modify</i> this worksheet you may not distribute your modified version (outside your own teaching institution) without the author's permission.
+        Please <a href="mailto:rpawson@nakedobjects.org" target="_blank">email the author</a> to report errors or suggest improvements.
+    </div>
+  </div>
+  <div hidden="" id="hash"></div>
+  <div hidden="" id="changes"></div>
+</step>`;
+}
+
 async function processWorksheet(fileName: string) {
-  const source = loadFileAsSourceNew(fileName);
+  let source = loadFileAsSourceNew(fileName);
+  source = prependFirstStep(source);
+  source = appendLastStep(source);
 
   let updatedContent = await processCode(source);
   updatedContent = await processSteps(updatedContent);
+
+  updatedContent = wrapInWorkSheetBoilerPlate(updatedContent);
 
   updatedContent = toDiffableHtml(updatedContent);
 
