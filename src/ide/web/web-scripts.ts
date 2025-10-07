0a00fefacb5a2465f21827f898f489993bc90fec
Stefano Cascarini <stef.cascarini@googlemail.com>
2025-10-07 16:50:23 +0100

    #1923

--- src/ide/web/web-scripts.ts
+++ src/ide/web/web-scripts.ts
@@ -2603,8 +2603,9 @@ function handleClickDropDownButton(event: Event) {
   const button = event.target as HTMLButtonElement;
   const isExpanded = button.getAttribute("aria-expanded") === "true";
   const menuId = button.getAttribute("aria-controls")!;
+  const menu = document.getElementById(menuId)!;
   button.setAttribute("aria-expanded", `${!isExpanded}`);
-  document.getElementById(menuId)!.hidden = isExpanded;
+  menu.hidden = isExpanded;
 
   const allDropDowns = document.querySelectorAll(
     "button[aria-haspopup='true']",
@@ -2615,6 +2616,10 @@ function handleClickDropDownButton(event: Event) {
       collapseMenu(e, false);
     }
   }
+
+  const firstitem = menu.querySelector(".menu-item") as HTMLElement;
+  firstitem.focus();
+
   event.stopPropagation();
 }
 
