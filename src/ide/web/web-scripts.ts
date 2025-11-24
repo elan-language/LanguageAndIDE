d56863f877285ba726aaf675cf6508d752f7eb7c
Richard Pawson <rpawson@nakedobjects.org>
2025-11-24 12:39:04 +0000

    #2172 & #2176

--- src/ide/web/web-scripts.ts
+++ src/ide/web/web-scripts.ts
@@ -1007,7 +1007,7 @@ function updateDisplayValues() {
     }
 
     if (canUndo()) {
-      enable(undoButton, "Undo last change (Ctrl-z)");
+      enable(undoButton, "Undo last change ");
     } else {
       disable([undoButton], "Nothing to undo");
     }
@@ -1015,7 +1015,7 @@ function updateDisplayValues() {
     if (nextFileIndex === -1) {
       disable([redoButton], "Nothing to redo");
     } else {
-      enable(redoButton, "Redo last change (Ctrl-y)");
+      enable(redoButton, "Redo last change ");
     }
 
     if (autoSaveFileHandle) {
