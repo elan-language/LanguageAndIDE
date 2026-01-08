38f5ab8c4883e100a9020d2e687def15bfcc28d2
Richard Pawson <rpawson@nakedobjects.org>
2026-01-08 16:14:05 +0000

    #2355 in .elan files replace all print instructions with call printLine(...)

--- test/testHelpers.ts
+++ test/testHelpers.ts
@@ -34,7 +34,7 @@ import { getTestRunner } from "./runner";
 
 
 // flag to update test file
-const updateTestFiles = false;
+const updateTestFiles = true;
 
 export async function assertEffectOfActionNew(
   sourceFile: string,
