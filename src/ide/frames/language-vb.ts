47b70721b934302f69baf76663199cb60f3c0da9
Richard Pawson <rpawson@nakedobjects.org>
2026-01-16 16:42:47 +0000

    Add function annotation for C# & VB

--- src/ide/frames/language-vb.ts
+++ src/ide/frames/language-vb.ts
@@ -13,6 +13,7 @@ import { AbstractClass } from "./globals/abstract-class";
 import { ConcreteClass } from "./globals/concrete-class";
 import { ConstantGlobal } from "./globals/constant-global";
 import { Enum } from "./globals/enum";
+import { FunctionFrame } from "./globals/function-frame";
 import { GlobalComment } from "./globals/global-comment";
 import { GlobalFunction } from "./globals/global-function";
 import { GlobalProcedure } from "./globals/global-procedure";
@@ -63,6 +64,7 @@ export class LanguageVB implements Language {
     if (
       frame instanceof VariableStatement ||
       frame instanceof ProcedureFrame ||
+      frame instanceof FunctionFrame ||
       frame instanceof CallStatement ||
       frame instanceof SetStatement
     ) {
