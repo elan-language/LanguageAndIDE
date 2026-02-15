134e17311b6332c37d261012bfca7fef0108d309
Richard Pawson <rpawson@nakedobjects.org>
2026-02-15 14:52:06 +0000

    #2510

--- src/ide/frames/parse-nodes/type-simple-name.ts
+++ src/ide/frames/parse-nodes/type-simple-name.ts
@@ -1,9 +1,10 @@
 import { Regexes } from "../fields/regexes";
 import { File } from "../frame-interfaces/file";
 import { TokenType } from "../symbol-completion-helpers";
-import { RegExMatchNode } from "./regex-match-node";
+import { AbstractParseNode } from "./abstract-parse-node";
+import { matchRegEx } from "./parse-node-helpers";
 
-export class TypeSimpleName extends RegExMatchNode {
+export class TypeSimpleName extends AbstractParseNode {
   tokenTypes: Set<TokenType> = new Set<TokenType>();
 
   constructor(
@@ -15,10 +16,23 @@ export class TypeSimpleName extends RegExMatchNode {
       TokenType.type_enum,
     ]),
   ) {
-    super(file, Regexes.typeSimpleName);
+    super(file);
     this.completionWhenEmpty = "<i>Type</i>";
     this.tokenTypes = tokenTypes;
   }
+
+
+  parseText(text: string): void {
+    this.remainingText = text;
+    if (text.length > 0) {
+      // else
+      [this.status, this.matchedText, this.remainingText] = matchRegEx(text, Regexes.typeSimpleName);
+    }
+    //test for matching any of the standard types specified in the lang & parse valid or incomplete
+    //else test for match of Elan standard type names and if so parse invalid
+    //else test for anything matching Regexes.typeSimpleName
+  }
+
   renderAsHtml(): string {
     return `<el-type>${this.matchedText}</el-type>`;
   }
