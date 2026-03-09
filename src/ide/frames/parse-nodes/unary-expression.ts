ce7db02958ff58a4fb8bf32ddf801bae2c3a55ff
Richard Pawson <rpawson@nakedobjects.org>
2026-03-09 13:03:07 +0000

    First attempt - working except for VB parse

--- src/ide/frames/parse-nodes/unary-expression.ts
+++ src/ide/frames/parse-nodes/unary-expression.ts
@@ -2,11 +2,8 @@ import { File } from "../frame-interfaces/file";
 import { MINUS } from "../symbols";
 import { AbstractSequence } from "./abstract-sequence";
 import { Alternatives } from "./alternatives";
-import { KeywordNode } from "./keyword-node";
-import { Space } from "./parse-node-helpers";
+import { NotOperator } from "./notOperator";
 import { PunctuationNode } from "./punctuation-node";
-import { Sequence } from "./sequence";
-import { SpaceNode } from "./space-node";
 import { Term } from "./term";
 
 export class UnaryExpression extends AbstractSequence {
@@ -19,12 +16,9 @@ export class UnaryExpression extends AbstractSequence {
 
   parseText(text: string): void {
     if (text.length > 0) {
-      const lang = this.file.language();
       const minus = () => new PunctuationNode(this.file, MINUS);
-      const not = () => new KeywordNode(this.file, lang.NOT);
-      const sp = () => new SpaceNode(this.file, Space.added);
-      const notSp = () => new Sequence(this.file, [not, sp]);
-      this.unaryOp = new Alternatives(this.file, [minus, notSp]);
+      const not = () => new NotOperator(this.file, (lang) => lang.NOT);
+      this.unaryOp = new Alternatives(this.file, [minus, not]);
       this.addElement(this.unaryOp);
       this.term = new Term(this.file);
       this.addElement(this.term);
