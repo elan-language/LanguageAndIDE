9b33ed5851fecb6f3c34d1fc84e8d02a6edf47c4
Richard Pawson <rpawson@nakedobjects.org>
2026-05-14 12:43:30 +0100

    #3007 more file updates

--- test/parsing/parse-source-and-generate-html.test.ts
+++ test/parsing/parse-source-and-generate-html.test.ts
@@ -1,108 +1,7 @@
 import { ignore_test } from "../compiler/compiler-test-helpers";
-import { assertEffectOfActionNew, assertGeneratesHtmlSourceAndExportFiles } from "../testHelpers";
-import {
-  CollapseAll,
-  ExpandAll,
-  SelectMainById,
-  SelectStatementById,
-} from "./model-generating-functions";
+import { assertGeneratesHtmlSourceAndExportFiles } from "../testHelpers";
 
 suite("Parse source and generate Html", () => {
-  test("Test Empty File", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T00_emptyFile.elan`,
-      `./out/test/files/T00_emptyFile.html`,
-    );
-  });
-
-  test("Test Hello World", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T01_helloWorld.elan`,
-      `./out/test/files/T01_helloWorld.html`,
-    );
-  });
-
-  test("Test Comments", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T02_comments.elan`,
-      `./out/test/files/T02_comments.html`,
-    );
-  });
-
-  test("Test Main With All Statements", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T03_mainWithAllStatements.elan`,
-      `./out/test/files/T03_mainWithAllStatements.html`,
-    );
-  });
-
-  test("Test All Globals Except Class", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T04_allGlobalsExceptClass.elan`,
-      `./out/test/files/T04_allGlobalsExceptClass.html`,
-    );
-  });
-
-  test("Test Classes", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/T05_classes.elan`,
-      `./out/test/files/T05_classes.html`,
-    );
-  });
-
-  test("Test Select Main By Id", async () => {
-    await assertEffectOfActionNew(
-      `./out/test/files/T03_mainWithAllStatements.elan`,
-      SelectMainById,
-      `./out/test/files/T07_mainSelected.html`,
-    );
-  });
-
-  test("Test Select Statement By Id", async () => {
-    await assertEffectOfActionNew(
-      `./out/test/files/T03_mainWithAllStatements.elan`,
-      SelectStatementById,
-      `./out/test/files/T07_statementSelected.html`,
-    );
-  });
-
-  //-------------------
-  test("ExpandAll", async () => {
-    await assertEffectOfActionNew(
-      `./out/test/files/T03_mainWithAllStatements.elan`,
-      ExpandAll,
-      `./out/test/files/T08_expandAll.html`,
-    );
-  });
-
-  test("CollapseAll", async () => {
-    await assertEffectOfActionNew(
-      `./out/test/files/T03_mainWithAllStatements.elan`,
-      CollapseAll,
-      `./out/test/files/T08_collapseAll.html`,
-    );
-  });
-
-  test("#702 - last line is a comment", async () => {
-    await assertEffectOfActionNew(
-      `./out/test/files/test702.elan`,
-      CollapseAll,
-      `./out/test/files/test702.html`,
-    );
-  });
-
-  ignore_test("Test Newline in literal string", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/test1762newlineInString.elan`,
-      `./out/test/files/test1762newlineInString.html`,
-    );
-  });
-  ignore_test("Ghosting", async () => {
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/ghosted.elan`,
-      `./out/test/files/ghosted.html`,
-    );
-  });
   test("Burrow", async () => {
     await assertGeneratesHtmlSourceAndExportFiles(
       `./out/test/files/burrow.elan`,
@@ -151,17 +50,6 @@ suite("Parse source and generate Html", () => {
       `./out/test/files/collatz.vb`,
       `./out/test/files/collatz.cs`,
       `./out/test/files/collatz.java`,
-    );
-  });
-  ignore_test("Wordle Demo", async () => {
-    // ignored only because it is slow for a test due to size
-    await assertGeneratesHtmlSourceAndExportFiles(
-      `./out/test/files/wordle-demo.elan`,
-      ``,
-      `./out/test/files/wordle-demo.py`,
-      `./out/test/files/wordle-demo.vb`,
-      `./out/test/files/wordle-demo.cs`,
-      //`./out/test/files/wordle-demo.java`,   Works live, but not in test!
     );
   });
   test("Life", async () => {
