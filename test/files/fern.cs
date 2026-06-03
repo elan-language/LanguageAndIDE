6649b64d5107d7ea3cf87d12882e8be3c9387328
Charles Wicksteed <charles.wicksteed@lycos.com>
2026-06-03 21:21:26 +0000

    Update export test files to match new Fern demo #3042
    Having updated the Fern demo, the expected export output has changed.
    I have not looked at the files in detail.

--- test/files/fern.cs
+++ test/files/fern.cs
@@ -6,38 +6,36 @@
 
 static void main() {
   var vg = new List<VectorGraphic>();
-  // coordinates
-  var x = 0.0;
-  var y = 0.16;
-  // random number
-  var r = new Random();
-  r.initialiseFromClock(); // call procedure
-  var rect = new RectangleVG();
-  var count = 0;
+  var s = new State(new RectangleVG(), 0.0, 0.16, new Random());
+  // can't call s.r.initialiseFromClock() directly
+  var t = s.r;
+  t.initialiseFromClock(); // call procedure
   // don't set the count limit too high
   // or the browser will run out of memory
-  while (count < 1000) {
-    var point = onepoint(x, y, r);
-    rect = point.item_0; // reassign variable
-    x = point.item_1; // reassign variable
-    y = point.item_2; // reassign variable
-    r = point.item_3; // reassign variable
-    vg.append(rect); // call procedure
+  foreach (count in range(0, 1000)) {
+    s = onepoint(s); // reassign variable
+    vg.append(s.rect); // call procedure
     displayVectorGraphics(vg); // call procedure
-    count = count + 1; // reassign variable
-  } // while
+  } // foreach
   print("Finished");
 } // main
 
-static (RectangleVG, double, double, Random) onepoint(double x, double y, Random r) { // function
-  // next pseudo-random number (Grogono parameters)
-  var rValue = r.asFloat();
-  var rNext = r.nextGen();
-  var nx_ny = onestep(x, y, rValue);
-  var nx = nx_ny.item_0;
-  var ny = nx_ny.item_1;
-  var rect = (new RectangleVG()).withX(nx*scale + 50).withY(75 - ny*scale).withWidth(0.5).withHeight(0.5).withFillColour(0x408040).withFillColour(0x408040).withStrokeWidth(0.25);
-  return (rect, nx, ny, rNext);
+// The State class is used as a convenient way to pass multiple
+
+// items in and out.
+
+// The RectangleVG s.rect is ignored when passed in;
+
+// it is just the RectangleVG from last time.
+
+static State onepoint(State s) { // function
+  // s.r.asFloat() is the current random number between 0 and 1
+  var nx_ny = onestep(s.x, s.y, s.r.asFloat()); // let
+  var nx = nx_ny.item_0; // let
+  var ny = nx_ny.item_1; // let
+  var rect = (new RectangleVG()).withX(nx*scale + 50).withY(75 - ny*scale).withWidth(0.5).withHeight(0.5).withFillColour(0x408040).withFillColour(0x408040).withStrokeWidth(0.25); // let
+  // s.r.nextGen() moves the random number generator on to the next number
+  return (new State(rect, nx, ny, s.r.nextGen()));
 } // function
 
 // r is random Float 0.0 <= r < 1.0
@@ -50,44 +48,76 @@ static (double, double) onestep(double x, double y, double r) { // function
   // Currently you can't make the params into one long line
   // as it makes the browser use 100% CPU
   // xx xy yx yy cx cy probablity
-  var p1 = [0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01];
-  var p2 = [0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85];
-  var p3 = [0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07];
-  var p4 = [-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07];
-  var allPs = [p1, p2, p3, p4];
-  // cumulative probabilty
-  var cumuprob = 0.0;
-  var done = false;
-  var nx = 0.0;
-  var ny = 0.0;
-  foreach (pp in allPs) {
-    cumuprob = cumuprob + pp[6]; // reassign variable
-    if ((!done) && (r < cumuprob)) {
-      nx = x*pp[0] + y*pp[1] + pp[4]; // reassign variable
-      ny = x*pp[2] + y*pp[3] + pp[5]; // reassign variable
-      done = true; // reassign variable
-    } // if
-  } // foreach
-  return (nx, ny);
+  var p1 = [0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01]; // let
+  var p2 = [0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85]; // let
+  var p3 = [0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07]; // let
+  var p4 = [-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07]; // let
+  var allPs = [p1, p2, p3, p4]; // let
+  // use the random number r to select one of p1 to p4
+  // weighted by the probabilities and put it in pp
+  var pp = getparams(r, allPs, 0.0); // let
+  return (x*pp[0] + y*pp[1] + pp[4], x*pp[2] + y*pp[3] + pp[5]);
 } // function
+
+// select a parameter set depending on random number
+
+// check the first element, cumulate the probability
+
+// return the element when the cumulative probability reaches r
+
+// otherwise recurse with the rest of the list of params
+
+static List<double> getparams(double r, List<List<double>> prms, double cumuprob) { // function
+  var head = prms.head(); // let
+  var cp2 = cumuprob + head[6]; // let
+  return if(r < cp2, head, getparams(r, prms.tail(), cp2));
+} // function
+
+// class to hold the working data passed in and out of onepoint()
+
+class State {
+
+  public State(RectangleVG rect, double x, double y, Random r) {
+    this.rect = rect; // reassign variable
+    this.x = x; // reassign variable
+    this.y = y; // reassign variable
+    this.r = r; // reassign variable
+  } // constructor
+
+  public RectangleVG rect {get; private set;} // property
+
+  public double x {get; private set;} // property
+
+  public double y {get; private set;} // property
+
+  public Random r {get; private set;} // property
+
+  public string toString() { // function method
+    return $"rect at {this.rect.x}, {this.rect.y}";
+  } // function method
+
+} // class
 
 [TestMethod] static void test_one() {
   Assert.AreEqual([0.0064, 1.736], roundtuple2(onestep(0.0, 0.16, 0.5)))
   Assert.AreEqual([-0.0416, 1.6352], roundtuple2(onestep(0.0, 0.16, 0.9)))
+  var s = onepoint(new State(new RectangleVG(), 0.0, 0.16, new Random())); // let
+  Assert.AreEqual("rect at 50.0448, 62.848", s.toString())
+  var p1 = [0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01]; // let
+  var p2 = [0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85]; // let
+  Assert.AreEqual(p1, getparams(0.005, [p1, p2], 0.0))
+  Assert.AreEqual(p2, getparams(0.03, [p1, p2], 0.0))
+  Assert.AreEqual(p2, getparams(0.03, [p2], 0.01))
 } // test
 
 // two approaches to rounding a tuple to N decimal places
 
 static (double, double) roundtuple1((double, double) n) { // function
-  var a = n.item_0;
-  var b = n.item_1;
-  return (a.round(8), b.round(8));
+  return (n.item_0.round(8), n.item_1.round(8));
 } // function
 
 static List<double> roundtuple2((double, double) n) { // function
-  var a = n.item_0;
-  var b = n.item_1;
-  return [a, b].map(double x => x.round(8));
+  return [n.item_0, n.item_1].map(double x => x.round(8));
 } // function
 
 const Int scale = 7
--- test/files/fern.vb
+++ test/files/fern.vb
@@ -6,38 +6,36 @@
 
 Sub main()
   Dim vg = New List(Of VectorGraphic)() ' variable definition
-  ' coordinates
-  Dim x = 0.0 ' variable definition
-  Dim y = 0.16 ' variable definition
-  ' random number
-  Dim r = New Random() ' variable definition
-  r.initialiseFromClock() ' call procedure
-  Dim rect = New RectangleVG() ' variable definition
-  Dim count = 0 ' variable definition
+  Dim s = New State(New RectangleVG(), 0.0, 0.16, New Random()) ' variable definition
+  ' can't call s.r.initialiseFromClock() directly
+  Dim t = s.r ' variable definition
+  t.initialiseFromClock() ' call procedure
   ' don't set the count limit too high
   ' or the browser will run out of memory
-  While count < 1000
-    Dim point = onepoint(x, y, r) ' variable definition
-    rect = point.item_0 ' reassign variable
-    x = point.item_1 ' reassign variable
-    y = point.item_2 ' reassign variable
-    r = point.item_3 ' reassign variable
-    vg.append(rect) ' call procedure
+  For Each count In range(0, 1000)
+    s = onepoint(s) ' reassign variable
+    vg.append(s.rect) ' call procedure
     displayVectorGraphics(vg) ' call procedure
-    count = count + 1 ' reassign variable
-  End While
+  Next count
   print("Finished")
 End Sub
 
-Function onepoint(x As Double, y As Double, r As Random) As (RectangleVG, Double, Double, Random)
-  ' next pseudo-random number (Grogono parameters)
-  Dim rValue = r.asFloat() ' variable definition
-  Dim rNext = r.nextGen() ' variable definition
-  Dim nx_ny = onestep(x, y, rValue) ' variable definition
-  Dim nx = nx_ny.item_0 ' variable definition
-  Dim ny = nx_ny.item_1 ' variable definition
-  Dim rect = (New RectangleVG()).withX(nx*scale + 50).withY(75 - ny*scale).withWidth(0.5).withHeight(0.5).withFillColour(&H408040).withFillColour(&H408040).withStrokeWidth(0.25) ' variable definition
-  Return (rect, nx, ny, rNext)
+' The State class is used as a convenient way to pass multiple
+
+' items in and out.
+
+' The RectangleVG s.rect is ignored when passed in;
+
+' it is just the RectangleVG from last time.
+
+Function onepoint(s As State) As State
+  ' s.r.asFloat() is the current random number between 0 and 1
+  Dim nx_ny = onestep(s.x, s.y, s.r.asFloat()) ' let
+  Dim nx = nx_ny.item_0 ' let
+  Dim ny = nx_ny.item_1 ' let
+  Dim rect = (New RectangleVG()).withX(nx*scale + 50).withY(75 - ny*scale).withWidth(0.5).withHeight(0.5).withFillColour(&H408040).withFillColour(&H408040).withStrokeWidth(0.25) ' let
+  ' s.r.nextGen() moves the random number generator on to the next number
+  Return (New State(rect, nx, ny, s.r.nextGen()))
 End Function
 
 ' r is random Float 0.0 <= r < 1.0
@@ -50,44 +48,76 @@ Function onestep(x As Double, y As Double, r As Double) As (Double, Double)
   ' Currently you can't make the params into one long line
   ' as it makes the browser use 100% CPU
   ' xx xy yx yy cx cy probablity
-  Dim p1 = {0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01} ' variable definition
-  Dim p2 = {0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85} ' variable definition
-  Dim p3 = {0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07} ' variable definition
-  Dim p4 = {-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07} ' variable definition
-  Dim allPs = {p1, p2, p3, p4} ' variable definition
-  ' cumulative probabilty
-  Dim cumuprob = 0.0 ' variable definition
-  Dim done = False ' variable definition
-  Dim nx = 0.0 ' variable definition
-  Dim ny = 0.0 ' variable definition
-  For Each pp In allPs
-    cumuprob = cumuprob + pp[6] ' reassign variable
-    If (Not done) And (r < cumuprob) Then
-      nx = x*pp[0] + y*pp[1] + pp[4] ' reassign variable
-      ny = x*pp[2] + y*pp[3] + pp[5] ' reassign variable
-      done = True ' reassign variable
-    End If
-  Next pp
-  Return (nx, ny)
+  Dim p1 = {0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01} ' let
+  Dim p2 = {0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85} ' let
+  Dim p3 = {0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07} ' let
+  Dim p4 = {-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07} ' let
+  Dim allPs = {p1, p2, p3, p4} ' let
+  ' use the random number r to select one of p1 to p4
+  ' weighted by the probabilities and put it in pp
+  Dim pp = getparams(r, allPs, 0.0) ' let
+  Return (x*pp[0] + y*pp[1] + pp[4], x*pp[2] + y*pp[3] + pp[5])
 End Function
+
+' select a parameter set depending on random number
+
+' check the first element, cumulate the probability
+
+' return the element when the cumulative probability reaches r
+
+' otherwise recurse with the rest of the list of params
+
+Function getparams(r As Double, prms As List(Of List(Of Double)), cumuprob As Double) As List(Of Double)
+  Dim head = prms.head() ' let
+  Dim cp2 = cumuprob + head[6] ' let
+  Return if(r < cp2, head, getparams(r, prms.tail(), cp2))
+End Function
+
+' class to hold the working data passed in and out of onepoint()
+
+Class State
+
+  Sub New(rect As RectangleVG, x As Double, y As Double, r As Random)
+    Me.rect = rect ' reassign variable
+    Me.x = x ' reassign variable
+    Me.y = y ' reassign variable
+    Me.r = r ' reassign variable
+  End Sub
+
+  Property rect As RectangleVG
+
+  Property x As Double
+
+  Property y As Double
+
+  Property r As Random
+
+  Function toString() As String
+    Return $"rect at {Me.rect.x}, {Me.rect.y}"
+  End Function
+
+End Class
 
 <TestMethod> Sub test_one()
   Assert.AreEqual({0.0064, 1.736}, roundtuple2(onestep(0.0, 0.16, 0.5)))
   Assert.AreEqual({-0.0416, 1.6352}, roundtuple2(onestep(0.0, 0.16, 0.9)))
+  Dim s = onepoint(New State(New RectangleVG(), 0.0, 0.16, New Random())) ' let
+  Assert.AreEqual("rect at 50.0448, 62.848", s.toString())
+  Dim p1 = {0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01} ' let
+  Dim p2 = {0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85} ' let
+  Assert.AreEqual(p1, getparams(0.005, {p1, p2}, 0.0))
+  Assert.AreEqual(p2, getparams(0.03, {p1, p2}, 0.0))
+  Assert.AreEqual(p2, getparams(0.03, {p2}, 0.01))
 End Sub
 
 ' two approaches to rounding a tuple to N decimal places
 
 Function roundtuple1(n As (Double, Double)) As (Double, Double)
-  Dim a = n.item_0 ' variable definition
-  Dim b = n.item_1 ' variable definition
-  Return (a.round(8), b.round(8))
+  Return (n.item_0.round(8), n.item_1.round(8))
 End Function
 
 Function roundtuple2(n As (Double, Double)) As List(Of Double)
-  Dim a = n.item_0 ' variable definition
-  Dim b = n.item_1 ' variable definition
-  Return {a, b}.map(Function (x As Double) x.round(8))
+  Return {n.item_0, n.item_1}.map(Function (x As Double) x.round(8))
 End Function
 
 Const scale = 7
