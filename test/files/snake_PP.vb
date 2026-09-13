949139a3c3fe0f01222889f5ce02bc31729af4cd
Richard Pawson <rpawson@nakedobjects.org>
2026-09-13 17:27:19 +0100

    Update snake PP

--- test/files/snake_PP.vb
+++ test/files/snake_PP.vb
@@ -2,36 +2,36 @@
 
 ' Use the w,a,s,d keys to change snake's direction
 
+Const width = 40
+
+Const height = 30
+
 Sub main()
   Dim bg = New BlockGraphics() ' variable definition
-  Dim head = 620 ' variable definition
-  Dim snake = {head, 619} ' variable definition
+  Dim head = 621 ' variable definition
+  Dim snake = {head - 1, head} ' variable definition
   Dim currentDir = "d" ' variable definition
   Dim gameOn = True ' variable definition
-  Dim apple = 0 ' variable definition
-  Dim newApple = True ' variable definition
+  Dim apple = -1 ' variable definition
   While gameOn
-    While newApple
-      apple = bg.calculateBlockNo(randint(0, 39), randint(0, 29)) ' assignment
-      If Not snake.contains(apple) Then
-        newApple = False ' assignment
-      End If
+    While (apple = -1) Or snake.contains(apple)
+      apple = randint(0, width*height) ' assignment
     End While
     updateDisplay(bg, snake, apple) ' procedure call
     Dim key = getKey() ' variable definition
     If Not key.equals("") And "wasd".contains(key) Then
       currentDir = key ' assignment
     End If
-    head = getAdjacentBlock(head, currentDir, bg) ' assignment
+    head = getAdjacentBlock(head, currentDir) ' assignment
     If (head = -1) Or snake.contains(head) Then
       gameOn = False ' assignment
     Else
-      snake.prepend(head) ' procedure call
+      snake.append(head) ' procedure call
     End If
     If head.equals(apple) Then
-      newApple = True ' assignment
+      apple = -1 ' assignment
     Else
-      snake.removeAt(snake.length() - 1) ' procedure call
+      snake.removeAt(0) ' procedure call
     End If
     sleep_ms(150) ' procedure call
   End While
@@ -47,34 +47,37 @@ Sub updateDisplay(bg As BlockGraphics, snake As List(Of Integer), apple As Integ
   displayBlockGraphics(bg) ' procedure call
 End Sub
 
-Function getAdjacentBlock(bl As Integer, dir As String, bg As BlockGraphics) As Integer
-  Dim newCol = bg.col(bl) ' variable definition
-  Dim newRow = bg.row(bl) ' variable definition
-  If dir.equals("a") Then
-    newCol = newCol - 1 ' assignment
-  ElseIf dir.equals("d") Then
-    newCol = newCol + 1 ' assignment
-  ElseIf dir.equals("w") Then
+Function getAdjacentBlock(bl As Integer, dir As String) As Integer
+  Dim adj = -1 ' variable definition
+  Dim newCol = bl Mod width ' variable definition
+  Dim newRow = divAsInt(bl, width) ' variable definition
+  If dir.equals("w") Then
     newRow = newRow - 1 ' assignment
+  ElseIf dir.equals("a") Then
+    newCol = newCol - 1 ' assignment
   ElseIf dir.equals("s") Then
     newRow = newRow + 1 ' assignment
+  ElseIf dir.equals("d") Then
+    newCol = newCol + 1 ' assignment
   End If
-  Return bg.calculateBlockNo(newCol, newRow)
+  If (newCol >= 0) And (newCol < width) And (newRow >= 0) And (newRow < height) Then
+    adj = newRow*width + newCol ' assignment
+  End If
+  Return adj
 End Function
 
 <TestClass Class Test_getAdjacentBlock
  <TestMethod> Sub test_getAdjacentBlock()
-  Dim bg = New BlockGraphics() ' variable definition
   Dim bl = 617 ' variable definition
-  Assert.AreEqual(577, getAdjacentBlock(bl, "w", bg))
-  Assert.AreEqual(657, getAdjacentBlock(bl, "s", bg))
-  Assert.AreEqual(616, getAdjacentBlock(bl, "a", bg))
-  Assert.AreEqual(618, getAdjacentBlock(bl, "d", bg))
+  Assert.AreEqual(577, getAdjacentBlock(bl, "w"))
+  Assert.AreEqual(616, getAdjacentBlock(bl, "a"))
+  Assert.AreEqual(657, getAdjacentBlock(bl, "s"))
+  Assert.AreEqual(618, getAdjacentBlock(bl, "d"))
   ' boundary
-  Assert.AreEqual(-1, getAdjacentBlock(20, "w", bg))
-  Assert.AreEqual(-1, getAdjacentBlock(1180, "s", bg))
-  Assert.AreEqual(-1, getAdjacentBlock(40, "a", bg))
-  Assert.AreEqual(-1, getAdjacentBlock(79, "d", bg))
+  Assert.AreEqual(-1, getAdjacentBlock(20, "w"))
+  Assert.AreEqual(-1, getAdjacentBlock(40, "a"))
+  Assert.AreEqual(-1, getAdjacentBlock(1180, "s"))
+  Assert.AreEqual(-1, getAdjacentBlock(79, "d"))
  End Sub
 End Class
 
